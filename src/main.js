91a57325fc3c764371590b6c77f4b95ba682f02c
Simon Edwards <simon@simonzone.com>
2014-05-08 21:22:37 +0200

    Use the application mode in our custom term.js.

--- src/main.js
+++ src/main.js
@@ -1,34 +1,50 @@
 /**
  * Copyright 2014 Simon Edwards <simon@simonzone.com>
  */
+
+var EXTRATERM_COOKIE_ENV = "EXTRATERM_COOKIE";
+
 exports.startUp = (function() {
   "use strict";
     
   function startUp() {
     var termjs = require('term.js');
     var child_process = require('child_process');
-
+    var cookie;
+    var htmldata = null;
+    
+    cookie = "DEADBEEF";  // FIXME
+    
     var term = new termjs.Terminal({
       cols: 80,
       rows: 30,
-      screenKeys: true
+      scrollback: 10000,
+      physicalScroll: true,
+      applicationModeCookie: cookie
     });
-
+    
+    term.debug = true;
+    
     term.on('title', function(title) {
       window.document.title = title;
     });
 
     term.open(window.document.body);
+    
+    window.addEventListener('resize', function() {
+      var size = term.resizeToContainer();
+      sendResize(size.cols, size.rows);
+    });
+    
+    term.write('\x1b[31mWelcome to extraterm!\x1b[m\r\n');
+    
+    process.env[EXTRATERM_COOKIE_ENV] = cookie;
 
-    term.write('\x1b[31mWelcome to term.js!\x1b[m\r\n');
-
-//    socket.on('data', function(data) {
-//      term.write(data);
-//    });
-
-    var bridge = child_process.spawn('node', ['pty_bridge.js']);
+    // Start our PTY bridge process.
+    var bridge = child_process.spawn('node', ['pty_bridge.js'], {
+      env: process.env
+    });
     bridge.stdout.on('data', function (data) {
-//      console.log("Incoming: "+data);
       term.write("" + data);
     });
 
@@ -42,8 +58,8 @@ exports.startUp = (function() {
     
     function sendData(text, callback) {
       var jsonString = JSON.stringify({stream: text});
-      console.log("<<< json string is ",jsonString);
-      console.log("<<< json string length is ",jsonString.length);
+//      console.log("<<< json string is ",jsonString);
+//      console.log("<<< json string length is ",jsonString.length);
       var sizeHeaderBuffer = new Buffer(4);
       sizeHeaderBuffer.writeUInt32BE(jsonString.length, 0);
 
@@ -53,8 +69,8 @@ exports.startUp = (function() {
 
     function sendResize(cols, rows, callback) {
       var jsonString = JSON.stringify({resize: [cols, rows]});
-      console.log("<<< json string is ",jsonString);
-      console.log("<<< json string length is ",jsonString.length);
+//      console.log("<<< json string is ",jsonString);
+//      console.log("<<< json string length is ",jsonString.length);
       var sizeHeaderBuffer = new Buffer(4);
       sizeHeaderBuffer.writeUInt32BE(jsonString.length, 0);
 
@@ -65,6 +81,27 @@ exports.startUp = (function() {
     term.on('data', function(data) {
       sendData(data);
     });
+    
+    term.on('application-mode-start', function(params) {
+      console.log("application-mode started! ",params);
+      htmldata = "";
+    });
+
+    term.on('application-mode-data', function(data) {
+      console.log("html-mode data!", data);
+      htmldata = htmldata + data;
+    });
+    term.on('application-mode-end', function() {
+      var el = window.document.createElement("div");
+      el.innerHTML = htmldata;
+      term.appendElement(el);
+      htmldata = null;
+      console.log("html-mode end!");
+    });
+    
+    var size = term.resizeToContainer();
+    sendResize(size.cols, size.rows);
+
   }
     
   return startUp;
