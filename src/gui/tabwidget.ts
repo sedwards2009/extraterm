ec3d8672d2eca3f40728263b3f8e1797b542fcaa
Simon Edwards <simon@simonzone.com>
2015-05-25 09:53:13 +0200

    Added support to the tabwidget for showing a vistual frame which contains the tab contents.

--- src/gui/tabwidget.ts
+++ src/gui/tabwidget.ts
@@ -6,6 +6,7 @@ CbStackedWidget.init();
 
 const ID = "CbTabWidgetTemplate";
 const ATTR_TAG = 'data-cb-tag';
+const ATTR_SHOW_FRAME = "show-frame";
 
 let registered = false;
 
@@ -32,75 +33,92 @@ class CbTabWidget extends HTMLElement {
       template.innerHTML = `
 <style>
 DIV.top {
-    display: flex;
-    flex-direction: column;
+  display: flex;
+  flex-direction: column;
 }
-
 DIV.tabbar {
-    background-color: #dfe2e2;
-    padding: 2px;
-
-    display: flex;
-    flex: 0 auto;
-    flex-direction: row;
-    
-    cursor: default;
-    -webkit-touch-callout: none;
-    -webkit-user-select: none;
+  display: flex;
+  flex: 0 auto;
+  flex-direction: row;
+  
+  cursor: default;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
 }
-      
+
+DIV.tabbar > DIV.tab + DIV.tab {
+  margin-left: 2px;
+}
+
 DIV.tab_active {
-    border-left: 1px solid #ffffff;
-    border-right: 1px solid #ffffff;
-    border-top: 1px solid #ffffff;
+  padding: 0 0.563rem;
+  font-size: 0.875rem;
+  line-height: 1.313rem;
+  letter-spacing: 0;
+  color: #454545;
+  text-shadow: 0 1px #fff;
+  vertical-align: top;
     
-    font-size: 0.875rem;
-    
-    border-top-left-radius: 4px;
-    border-top-right-radius: 4px;
-    
-    padding-top: 4px;
-    
-    background-color: #f4f4f4;
-    margin-left: 2px;
-    margin-right: 2px;
-    box-shadow: 0 -1px 1px -1px black, -1px 0px 1px -1px black, 1px 0px 1px -1px black;
-    
-    text-align: center;
-    flex-basis: 15rem;
-    
-    text-overflow: ellipsis;
-    white-space: pre;
-    overflow: hidden;
+  background-color: #dfe2e2;
+
+  box-shadow: inset 0 1px #fff;
+  border: 1px solid #9daca9;
+  border-bottom: 0px;
+  border-radius: 4px;
+  border-bottom-left-radius: 0px;
+  border-bottom-right-radius: 0px;
+
+  text-align: center;
+  flex-basis: 15rem;
+  
+  text-overflow: ellipsis;
+  white-space: pre;
+  overflow: hidden;
 }
 
 DIV.tab_inactive {
-    border-left: 1px solid #ffffff;
-    border-right: 1px solid #ffffff;
-    border-top: 1px solid #ffffff;
-    
-    font-size: 0.875rem;
-    
-    border-top-left-radius: 4px;
-    border-top-right-radius: 4px;
-    
-    padding-top: 2px;
-    
-    background-color: #D2D6D6;
-    margin-left: 2px;
-    margin-right: 2px;
-    box-shadow: 0 -1px 1px -1px black, -1px 0px 1px -1px black, 1px 0px 1px -1px black;
+  padding: 0 0.563rem;
+  font-size: 0.875rem;
+  line-height: 1.313rem;
+  letter-spacing: 0;
+  color: #454545;
+  text-shadow: 0 1px #fff;
+  vertical-align: top;
+  background-color: #e5e9e8;
+  box-shadow: inset 0 1px #fff;
+  border: 1px solid #9daca9;
+  border-bottom: 0px;
+  border-radius: 4px;
+  border-bottom-left-radius: 0px;
+  border-bottom-right-radius: 0px;
 
-    margin-top: 3px;
-    
-    text-align: center;
-    flex-basis: 15rem;
+  background-color: #D2D6D6;
 
-    text-overflow: ellipsis;
-    white-space: pre;
-    overflow: hidden;
+  margin-top: 3px;
+  
+  text-align: center;
+  flex-basis: 15rem;
+
+  text-overflow: ellipsis;
+  white-space: pre;
+  overflow: hidden;
 }
 
+DIV.tab_inactive:hover {
+  background-color: #eff1f1;
+}
+
+DIV.show_frame > DIV.tabbar > DIV.tab_inactive {
+  border-bottom: 1px solid #9daca9;
+}
+
+DIV.show_frame > #contentsstack {
+  border: 1px solid #9daca9;
+}
+DIV.show_frame > #tabbar {
+  position: relative;
+  top: 1px;
+}
 </style>\n
 <div id='top' class='top'>
 <div id='tabbar' class='tabbar'></div>
@@ -118,6 +136,10 @@ DIV.tab_inactive {
    */
   private __getById(id:string): Element {
     return util.getShadowRoot(this).querySelector('#'+id);
+  }
+  
+  private _getTop(): HTMLDivElement {
+    return <HTMLDivElement> this.__getById('top');
   }
   
   private _getTabbar(): HTMLDivElement {
@@ -138,8 +160,20 @@ DIV.tab_inactive {
     
     this.createTabHolders();
     this.currentIndex = 0;
+    this._showFrame(this.showFrame);
   }
   
+  attributeChangedCallback(attrName: string, oldValue: string, newValue: string): void {
+    switch (attrName) {
+      case ATTR_SHOW_FRAME:
+        this._showFrame(toBoolean(newValue));
+        break;
+        
+      default:
+        break;
+    }
+  }
+    
   private createTabHolders(): void {
     const tabbar = this._getTabbar();
     const contentsStack = this._getContentsStack();
@@ -164,6 +198,7 @@ DIV.tab_inactive {
     while (tabbar.childElementCount < tabCount) {
       // The tab part.
       const tabDiv = this.ownerDocument.createElement('div');
+      tabDiv.classList.add('tab');
       tabDiv.classList.add('tab_inactive');
       let contentElement = this.ownerDocument.createElement('content');
       contentElement.setAttribute('select', '[' + ATTR_TAG + '="tab_' + tabbar.childElementCount + '"]');
@@ -202,6 +237,26 @@ DIV.tab_inactive {
     return this._getContentsStack().currentIndex;
   }
   
+  set showFrame(value: boolean) {
+    this.setAttribute(ATTR_SHOW_FRAME, "" + value);
+  }
+  
+  get showFrame(): boolean {
+    if (this.hasAttribute(ATTR_SHOW_FRAME)) {
+      return toBoolean(this.getAttribute(ATTR_SHOW_FRAME));
+    } else {
+      return true;
+    }
+  }
+  
+  _showFrame(value: boolean): void {
+    if (value) {
+      this._getTop().classList.add('show_frame');
+    } else {
+      this._getTop().classList.remove('show_frame');          
+    }
+  }
+  
   private _showTab(index: number): void {
     const tabbar = this._getTabbar();
     for (let i=0; i<tabbar.children.length; i++) {
@@ -215,6 +270,22 @@ DIV.tab_inactive {
       }
     }
   }  
+}
+
+function toBoolean(value: any): boolean {
+  if (value === true || value === false) {
+    return value;
+  }
+  if (value === 0) {
+    return false;
+  }
+  if (value === 'true') {
+    return true;
+  }
+  if (value === 'false') {
+    return false;
+  }
+  return Boolean(value);
 }
 
 export = CbTabWidget;
