bcb6f1bf66edf6bf6d927ea56ed290561d2ec5c3
Simon Edwards <simon@simonzone.com>
2015-07-04 08:47:31 +0200

    Added a little non blocking reader class to the pty server/bridge. Added some commands.

--- src/bridge_test.js
+++ src/bridge_test.js
@@ -6,16 +6,15 @@ var spawn = require('child_process').spawn;
 var timers = require('timers');
 
 //var bridge = spawn('node', ['pty_bridge.js']);
-var pythonExe = "python3";
-// var pythonExe = 'C:\\Users\\sbe\\.babun\\cygwin\\bin\\python3.4m.exe'
+var pythonExe = process.platform === 'win32' ? 'C:\\Users\\sbe\\.babun\\cygwin\\bin\\python3.4m.exe' : 'python3';
 var bridge = spawn(pythonExe, ['python/ptyserver2.py']);
 
 bridge.stdout.on('data', function (data) {
-  console.log("bridge: "+data);
+  console.log("main <<< server : "+data);
 });
 
 bridge.stderr.on('data', function (data) {
-  console.log('bridge stderr: ' + data);
+  console.log('main <<< server stderr : ' + data);
 });
 
 bridge.on('close', function (code) {
@@ -24,18 +23,29 @@ bridge.on('close', function (code) {
 
 function sendData(text, callback) {
   var jsonString = JSON.stringify({stream: text});
-  console.log("<<< json string is ",jsonString);
+  console.log("main >>> server : " + jsonString);
 //  console.log("<<< json string length is ",jsonString.length);
 //  var sizeHeaderBuffer = new Buffer(4);
 //  sizeHeaderBuffer.writeUInt32BE(jsonString.length, 0);
   
 //  bridge.stdin.write(sizeHeaderBuffer);
-  bridge.stdin.write(jsonString+"\n\n", callback);
+  bridge.stdin.write(jsonString+"\n", callback);
+}
+
+function sendMessage(msg) {
+  var jsonString = JSON.stringify(msg);
+  console.log("main >>> server : ",jsonString);
+  bridge.stdin.write(jsonString+"\n");
 }
 
 var counter = 0;
+var todo = [
+  {type: "create", rows: 24, columns: 80, argv: ["/usr/bin/seq","3"]}
+];
 
 timers.setInterval(function() {
-  sendData('count '+counter+'\r');
+  if (counter < todo.length) {
+    sendMessage(todo[counter]);
+  }
   counter++;
 }, 1000);
--- src/python/ptyserver2.py
+++ src/python/ptyserver2.py
@@ -10,6 +10,7 @@ import os
 import codecs
 import array
 import threading
+import json
 
 def processPacket(byte_data):
     """
@@ -31,13 +32,16 @@ activity_event = threading.Event()
 nbfr_counter = 0
 
 class NonblockingFileReader:
-    def __init__(self, file_object):
+    def __init__(self, file_object=None, read=None):
         global nbfr_counter
         
         self.file_object = file_object
+        self._custom_read = read
         
         self.id = nbfr_counter
         nbfr_counter += 1
+        
+        self._isEOF = False
         
         self.buffer = []
         self.buffer_lock = threading.Lock()
@@ -59,18 +63,30 @@ class NonblockingFileReader:
         with self.buffer_lock:
             return len(self.buffer) != 0
 
+    def isEOF(self):
+        with self.buffer_lock:
+            return len(self.buffer)==0 and self._isEOF
+
     def _thread_start(self):
         global activity_event
-        while True:
-            chunk = self._read_next()
-            log("_thread_start read:" + repr(chunk))
-            with self.buffer_lock:
-                self.buffer.append(chunk)
-            # Tick the alarm
+        try:
+            while True:
+                chunk = self._read_next()
+                # log("_thread_start read:" + repr(chunk))
+                with self.buffer_lock:
+                    self.buffer.append(chunk)
+                # Tick the alarm
+                log("reading setting flag!")
+                activity_event.set()
+        except EOFError:
+            self._isEOF = True
             activity_event.set()
             
     def _read_next(self):
-        return self.file_object.read(10240)
+        if self._custom_read is not None:
+            return self._custom_read(1024)
+        else:
+            return self.file_object.read(10240)
 
 class NonblockingLineReader(NonblockingFileReader):
     def _read_next(self):
@@ -78,45 +94,110 @@ class NonblockingLineReader(NonblockingFileReader):
 
 def WaitOnIOActivity():
     global activity_event
+    # log("activity_event.wait()")
     activity_event.wait()
+    # log("activity_event.clear()")
     activity_event.clear()
 
 ###########################################################################
 
+pty_list = []   # List of dicts with structure {id: string, pty: pty, reader: }
+
+#
+#
+# Create pty command (from Extraterm process):
+# {
+#   type: string = "create";
+#   argv: string[];
+#   rows: number;
+#   columns: number;
+# }
+#
+# Created message (to Extraterm process):
+# {
+#   type: string = "created";
+#   id: string; // pty ID.
+# }
+#
+#
+# pty output message (to Extraterm process):
+# {
+#   type: string = "output";
+#   id: number; // pty ID.
+#   data: string;
+# }
+#
+# pty closed message (to Extraterm process):
+# {
+#   type: string = "closed";
+#   id: number; // pty ID.
+# }
+
+
+pty_counter = 1
+
+def process_command(json_command):
+    global pty_list
+    global pty_counter
+    
+    log("server process command:"+repr(json_command))
+    cmd = json.loads(json_command)
+    if cmd["type"] == "create":
+        # Create a new pty.
+        rows = cmd["rows"]
+        columns = cmd["columns"]
+        pty = ptyprocess.PtyProcess.spawn(cmd["argv"], dimensions=(rows, columns) ) #cwd=, env=, )
+        pty_reader = NonblockingFileReader(read=pty.read)
+        pty_id = pty_counter
+        pty_list.append( { "id": pty_id, "pty": pty, "reader": pty_reader } )
+        pty_counter += 1
+        
+        send_to_controller({ "type": "created", "id": pty_id })
+    else:
+        log("ptyserver receive unrecognized message:" + json_command)
+
+def send_to_controller(msg):
+    msg_text = json.dumps(msg)+"\n"
+    log("server >>> main : "+msg_text);
+    sys.stdout.write(msg_text)
+    sys.stdout.flush()
+
 def main():
-    # command_buffer = ""
+    global pty_list
+    
     log("pty server process starting up")
-    stdin_reader = NonblockingFileReader(sys.stdin.buffer.raw)
+    stdin_reader = NonblockingLineReader(sys.stdin)
     
     while True:
         WaitOnIOActivity()
+        log("Server awake")
+        
+        # Check the stdin control channel.
+        if stdin_reader.isEOF():
+            log("server <<< main : EOF")
+        
         chunk = stdin_reader.read()
         while chunk is not None:
-            log("Main <<< " + repr(chunk))
+            log("server <<< main : " + repr(chunk))
+            process_command(chunk.strip())
             chunk = stdin_reader.read()
+            
+        # Check our ptys for output.
+        for pty_struct in pty_list:
+            pty_chunk = pty_struct["reader"].read()
+            while pty_chunk is not None:
+                log("server <<< pty : " + repr(pty_chunk))
+                # Decode the chunk of bytes.
+                data = pty_chunk.decode(errors='ignore')
+                send_to_controller( {"type": "output", "id": pty_struct["id"], "data": data} )
+                pty_chunk = pty_struct["reader"].read()
 
-        # sendToMaster("Pty Server waiting\n")
-        # active_tup = select.select([sys.stdin.fileno()], [], [])
-        # sendToMaster("server saw something move!" + repr(active_tup))
-        # if sys.stdin.fileno() in active_tup[0]:
-        #     sendToMaster("server trying to read")
-        #     chunk = sys.stdin.readline(10240)
-        #     #chunk = sys.stdin.buffer.read(4)   # read up to 10Kb raw bytes.
-        #     sendToMaster("server reading: "+repr(chunk))
-        #     #command_buffer = command_buffer + chunk
-
-            #while len(command_buffer) >= 4:
-            #    size = struct.unpack(">I", command_buffer[0:4])[0]
-            #    sendToMaster("command size" + str(size))
-            #    if len(command_buffer) >= size + 4:
-            #        processPacket(command_buffer[4: size+4])
-            #        command_buffer = command_buffer[size+4:]
-            #    else:
-            #        break
-    #pty = ptyprocess.PtyProcess.spawn(['/bin/ls']) #cwd=, env=, dimensions=(24,80))
-    #result = select([pty.fileno()], [], []);
-    #if len(result[0]) != 0:
-    #    incoming = pty.read()
-#        print(incoming)
+        # Check for exitted ptys
+        for pty_struct in pty_list[:]:
+            log("checking live pty: "+str(pty_struct["pty"].isalive()))
+            if not pty_struct["pty"].isalive():
+                pty_list = [ t for t in pty_list if t["id"] != pty_struct["id"] ]
+                #pty_struct["reader"].close()
+                send_to_controller( {"type": "closed", "id": pty_struct["id"] } )
 
 main()
