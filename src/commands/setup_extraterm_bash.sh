52ccb6f25012c766adac1e2c73b7e6fb29f0f151
Simon Edwards <simon@simonzone.com>
2015-08-25 22:23:58 +0200

    fish shell integration and fixes for the bash integration.

--- src/commands/setup_extraterm_bash.sh
+++ src/commands/setup_extraterm_bash.sh
@@ -1,6 +1,5 @@
 # This file should be sourced from your .bashrc file.
 
-
 if [ -n "$EXTRATERM_COOKIE" ]; then
     echo "Setting up Extraterm support."
 
@@ -8,25 +7,31 @@ if [ -n "$EXTRATERM_COOKIE" ]; then
     filedir=`dirname "$BASH_SOURCE"`
     export PATH="$PWD/$filedir:$PATH"
 
+    postexec () {
+      echo -n -e "\033&${EXTRATERM_COOKIE};3\007"
+      echo -n $1
+      echo -n -e "\000"
+    }
+    export PROMPT_COMMAND="postexec \$?"
+
+    preexec () {
+        echo -n -e "\033&${EXTRATERM_COOKIE};2;bash\007"
+        echo -n $1
+        echo -n -e "\000"
+    }
+
+    preexec_invoke_exec () {
+        [ -n "$COMP_LINE" ] && return                     # do nothing if completing
+        [ "$BASH_COMMAND" = "$PROMPT_COMMAND" ] && return # don't cause a preexec for $PROMPT_COMMAND
+        local this_command=`history 1`; # obtain the command from the history
+        preexec "$this_command"
+    }
+    trap 'preexec_invoke_exec' DEBUG
+
     # Look for Python 3 support.
     if ! which python3 > /dev/null; then
         echo "Unable to find the Python3 executable!"
     else
-        # Found Python3
-        export PROMPT_COMMAND="exbashpostcommand.py \$?"
-
-        preexec () {
-            echo -n -e "\033&${EXTRATERM_COOKIE};2;bash\007$1\000"
-        }
-
-        preexec_invoke_exec () {
-            [ -n "$COMP_LINE" ] && return                     # do nothing if completing
-            [ "$BASH_COMMAND" = "$PROMPT_COMMAND" ] && return # don't cause a preexec for $PROMPT_COMMAND
-            local this_command=`history 1`; # obtain the command from the history
-            preexec "$this_command"
-        }
-        trap 'preexec_invoke_exec' DEBUG
-
         alias from="exfrom.py"
         alias show="exshow.py"
     fi
---
+++ src/commands/setup_extraterm_fish.fish
@@ -0,0 +1,30 @@
+# This file should be sourced from your ~/.config/config.fish file.
+
+if test -n "$EXTRATERM_COOKIE"
+    echo "Setting up Extraterm support."
+    
+    # Put our enhanced commands at the start of the PATH.
+    set -l filedir (dirname (status -f))
+    set -x PATH $PWD/$filedir $PATH
+
+    function extraterm_preexec -e fish_preexec
+      echo -n -e -s "\033&" $EXTRATERM_COOKIE ";2;fish\007"
+      echo -n $argv[1]
+      echo -n -e "\000"
+    end
+
+    function extraterm_postexec -e fish_postexec
+      set -l status_backup $status
+      echo -n -e -s "\033" "&" $EXTRATERM_COOKIE ";3\007"
+      echo -n $status_backup
+      echo -n -e "\000"
+    end
+    
+    function from 
+        exfrom.py $argv
+    end
+    
+    function show
+        exshow.py $argv
+    end
+end
