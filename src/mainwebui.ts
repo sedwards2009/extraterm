9f23a881207f37cf86fcbccf98aab8c7221b6c64
Simon Edwards <simon@simonzone.com>
2016-07-10 21:55:24 +0200

    Added an icon to the "select other pane" command.

--- src/mainwebui.ts
+++ src/mainwebui.ts
@@ -987,7 +987,7 @@ class ExtratermMainWebUI extends KeyBindingsElementBase {
       { id: COMMAND_SELECT_TAB_LEFT, label: "Select Previous Tab", target: target },
       { id: COMMAND_SELECT_TAB_RIGHT, label: "Select Next Tab", target: target },
       { id: COMMAND_NEW_TAB, iconRight: "plus", label: "New Tab", target: target },
-      { id: COMMAND_SELECT_OTHER_PANE, label: "Select other Pane", target: target },
+      { id: COMMAND_SELECT_OTHER_PANE, iconRight: "arrows-h",  label: "Select other Pane", target: target },
       { id: COMMAND_CLOSE_TAB, iconRight: "times", label: "Close Tab", target: target },
       { id: COMMAND_TOGGLE_SPLIT, iconLeft: this._split ? "check-square-o" : "square-o",
         iconRight: "columns", label: "Split", target: target },
