8279bbcf1be5673d8f0a2ee35e607d9caadbc27b
Simon Edwards <simon@simonzone.com>
2016-08-19 13:51:53 +0200

    Expanded the functionality of the thememanagerutility to also render themes to CSS and dump it in a directory for debugging/dev.

--- src/thememanagerutility.ts
+++ src/thememanagerutility.ts
@@ -3,34 +3,79 @@
  *
  * This source code is licensed under the MIT license which is detailed in the LICENSE.txt file.
  */
-
+ 
+/*
+ * This little utility can be run from the command line using node. It lets you view the metadata of all of the
+ * themes in the src/themes directory, and it can compile a theme into CSS and report any errors.
+ */
+import path = require('path');
+import fs = require('fs');
 import ThemeManager = require('./thememanager');
 import ThemeTypes = require('./theme');
+import SourceDir = require('./sourceDir');
+import commander = require('commander');
 
 import Logger = require('./logger');
 
 const print = console.log.bind(console);
 
+interface CommandLineOptions {
+  list?: boolean;
+  compile?: string;
+  output?: string;
+}
+
 function main(): void {
-  const tm = ThemeManager.makeThemeManager(['themes']);
-  tm.getAllThemes().filter( (themeInfo) => themeInfo.id === 'spacelab-ui').forEach( (themeInfo) => {
-    tm.renderThemes([themeInfo.id,'default']).then( (contents) => {
-      print("----");
-      print("ID:   ", themeInfo.id);
-      print("Name: ", themeInfo.name);
-      print("Path: ", themeInfo.path);
-      
-      
-      ThemeTypes.cssFileEnumItems.forEach( (item) => {
-        print("CSS " + ThemeTypes.cssFileNameBase(item) + "----");
-        if (contents.success) {
-          print(contents.themeContents.cssFiles[ThemeTypes.cssFileNameBase(item)]);
-        } else {
-          print(contents.errorMessage);
+  commander
+    .option('-l, --list', 'List all available themes.')
+    .option('-c, --compile [theme_id]', 'Compile a theme and write it to stdout.')
+    .option('-o, --output [directory]', 'Write compiled theme files to this directory instead of stdout.')
+    .parse(process.argv);
+  const options = <CommandLineOptions> commander;
+  
+  const tm = ThemeManager.makeThemeManager([path.join(SourceDir.path, 'themes')]);
+  const allThemes = tm.getAllThemes();
+  if (options.list) {
+    // List the theme metadata.
+    print(allThemes);
+  } else {
+  
+    const themes = allThemes.filter( (themeInfo) => themeInfo.id === options.compile);
+    if (themes.length === 0) {
+      print(`Unable to find theme with ID '${options.compile}'.`);
+      process.exit(1);
+    }
+    
+    themes.forEach( (themeInfo) => {
+      const outputDir = options.output;
+      if (outputDir !== undefined) {
+        try {
+          fs.accessSync(outputDir, fs.F_OK);
+        } catch(err) {
+          print(`Creating output directory '${outputDir}'`);
+          fs.mkdir(outputDir);
         }
+      }
+      
+      tm.renderThemes([themeInfo.id,'default']).then( (contents) => {
+        ThemeTypes.cssFileEnumItems.forEach( (item) => {
+          if (contents.success) {
+            if (outputDir !== undefined) {
+              const output = path.join(outputDir, ThemeTypes.cssFileNameBase(item) + '.css');
+              print(`Writing to ${output}`);
+              fs.writeFileSync(output, contents.themeContents.cssFiles[ThemeTypes.cssFileNameBase(item)]);
+              
+            } else {
+              print("/* CSS " + ThemeTypes.cssFileNameBase(item) + " */");
+              print(contents.themeContents.cssFiles[ThemeTypes.cssFileNameBase(item)]);
+            }
+          } else {
+            print(contents.errorMessage);
+          }
+        });
       });
     });
-  });
+  }
 }
 
 main();
