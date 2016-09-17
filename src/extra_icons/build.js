22e9c166bbad9a29bb5b77086e23929e79bef619
Simon Edwards <simon@simonzone.com>
2016-09-17 10:37:18 +0200

    Added some custom icons for minimize and maximize and baked them into a TTF.

---
+++ src/extra_icons/build.js
@@ -0,0 +1,30 @@
+/**
+ * Assemble svg images into a TTF.
+ *
+ * This should be run via npm script.
+ *  
+ * Output is to extraicons.ttf. The first code point in the font is U+EA01.
+ */
+
+const webfont = require('webfont').default;
+const fs = require('fs');
+
+function main() {
+  webfont({
+    files: [
+      'src/extra_icons/maximize.svg',
+      'src/extra_icons/minimize.svg'
+    ],
+    fontName: 'extraicons'
+  })
+  .then((result) => {
+      console.log("Done!");
+      console.log(result);
+
+      fs.writeFileSync("src/extra_icons/extraicons.ttf", result.ttf);
+  },  (ex) => { // Exception reporting.
+      console.log(ex);
+  });
+}
+
+main();
