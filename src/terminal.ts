b05acdffc645e6a48a939513f374aff748174a19
Simon Edwards <simon@simonzone.com>
2016-10-14 10:41:58 +0200

    Added a reminder to fix the frame fetching and make it binary safe.

--- src/terminal.ts
+++ src/terminal.ts
@@ -1668,7 +1668,7 @@ class EtTerminal extends ThemeableElementBase implements CommandPaletteRequestTy
     } else {
       data = this.getFrameContents(frameId);
     }
-    
+    // FIXME this should be binary safe and send metadata like mimetype and charset.
     const lines = data.split("\n");
     let encodedData: string = "";
     lines.forEach( (line: string) => {
