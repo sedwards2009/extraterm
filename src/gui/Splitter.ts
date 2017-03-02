341f5a9fac2f3d4fcfd613df6426a1af0cf5ce12
Simon Edwards <simon@simonzone.com>
2017-03-02 21:48:07 +0100

    The splitter bar can now be dragged.

--- src/gui/Splitter.ts
+++ src/gui/Splitter.ts
@@ -29,6 +29,7 @@ const CLASS_DRAG = "CLASS_DRAG";
 
 const DIVIDER_WIDTH = 10;
 const NOT_DRAGGING = -1;
+const MIN_PANE_WIDTH = 32;
 
 /**
  * A widget to display panes of widgets separated by a moveable gap/bar.
@@ -61,12 +62,15 @@ export class Splitter extends ThemeableElementBase {
   
   private _mutationObserver: MutationObserver;
   
+  private _paneWidths: number[];
+
   private _dividerDrag: number; // The number of the divider currently being dragged. -1 means not dragging.
 
   private _dividerDragOffsetX: number;
 
   private _initProperties(): void {
     this._log = new Logger(Splitter.TAG_NAME, this);
+    this._paneWidths = [];
     this._mutationObserver = null;
     this._dividerDrag = NOT_DRAGGING;
     this._dividerDragOffsetX = 0;
@@ -94,6 +98,7 @@ export class Splitter extends ThemeableElementBase {
     shadow.appendChild(clone);
     this.updateThemeCss();
     
+    this._paneWidths = this._equalPaneWidths();
     this._createLayout();
 
     const topDiv = DomUtils.getShadowId(this, ID_TOP);
@@ -101,6 +106,9 @@ export class Splitter extends ThemeableElementBase {
     topDiv.addEventListener('mousedown', this._handleMouseDown.bind(this));
     topDiv.addEventListener('mouseup', this._handleMouseUp.bind(this));
     topDiv.addEventListener('mousemove', this._handleMouseMove.bind(this));
+
+    const coverDiv = DomUtils.getShadowId(this, ID_COVER);
+    coverDiv.addEventListener('mouseleave', this._handleMouseLeave.bind(this));
 
     const indicatorDiv = DomUtils.getShadowId(this, ID_INDICATOR);
     indicatorDiv.style.width = "" + DIVIDER_WIDTH + "px";
@@ -118,8 +126,9 @@ export class Splitter extends ThemeableElementBase {
   //-----------------------------------------------------------------------
 
   update(): void {
+    this._paneWidths = this._equalPaneWidths();
     this._createLayout();
-  }  
+  }
   
   bulkRefresh(level: ResizeRefreshElementBase.RefreshLevel): BulkDomOperation.BulkDOMOperation {
     // const contentsStack = this._getContentsStack();
@@ -140,13 +149,26 @@ export class Splitter extends ThemeableElementBase {
       template.innerHTML = `<style id="${ThemeableElementBase.ID_THEME}"></style>
 <div id='${ID_TOP}'>
 <div id='${ID_CONTAINER}'></div>
-<div id='${ID_COVER}'></div><div id='${ID_INDICATOR}'></div>
+<div id='${ID_COVER}'><div id='${ID_INDICATOR}'></div></div>
 </div>`;
       window.document.body.appendChild(template);
     }
     
     return window.document.importNode(template.content, true);
   }
+
+  //-----------------------------------------------------------------------
+  //
+  //   #######                                   
+  //   #       #    # ###### #    # #####  ####  
+  //   #       #    # #      ##   #   #   #      
+  //   #####   #    # #####  # #  #   #    ####  
+  //   #       #    # #      #  # #   #        # 
+  //   #        #  #  #      #   ##   #   #    # 
+  //   #######   ##   ###### #    #   #    ####  
+  //
+  //-----------------------------------------------------------------------
+                                           
 
   private _handleMouseDown(ev: MouseEvent): void {
     const target = <HTMLElement> ev.target;
@@ -156,6 +178,9 @@ export class Splitter extends ThemeableElementBase {
     if ( ! target.classList.contains(CLASS_DIVIDER)) {
       return;
     }
+
+    ev.preventDefault();
+    ev.stopPropagation();
 
     // Figure out which divider was hit.
     const containerDiv = DomUtils.getShadowId(this, ID_CONTAINER);
@@ -193,13 +218,21 @@ export class Splitter extends ThemeableElementBase {
     this._log.debug("target: ", target);
     this._log.debug("has divider: ", target.classList.contains(CLASS_DIVIDER));
 
+    ev.preventDefault();
+    ev.stopPropagation();
+
+    this._stopDrag();
+  }
+
+  private _stopDrag(): void {
     const topDiv = DomUtils.getShadowId(this, ID_TOP);
-    if (topDiv.classList.contains(CLASS_NORMAL)) {
-      return; // Nothing to do.
+    if (this._dividerDrag === NOT_DRAGGING) {
+      return;
     }
 
     topDiv.classList.remove(CLASS_DRAG);
     topDiv.classList.add(CLASS_NORMAL);
+    this._dividerDrag = NOT_DRAGGING;
   }
 
   private _handleMouseMove(ev: MouseEvent): void {
@@ -207,11 +240,44 @@ export class Splitter extends ThemeableElementBase {
       return;
     }
 
+    if ((ev.buttons & 1) === 0) {
+      this._stopDrag();
+      return;
+    }
+
     const topDiv = DomUtils.getShadowId(this, ID_TOP);
     const indicatorDiv = DomUtils.getShadowId(this, ID_INDICATOR);
     const topRect = topDiv.getBoundingClientRect();
-    indicatorDiv.style.left = "" + (ev.clientX - topRect.left - this._dividerDragOffsetX) + "px";
+    const newIndicatorLeft = ev.clientX - topRect.left - this._dividerDragOffsetX;
+    const newPaneWidths = this._adjustPaneWidth(this._paneWidths, this._dividerDrag, newIndicatorLeft);
+
+    indicatorDiv.style.left = "" + this._paneRight(newPaneWidths, this._dividerDrag) + "px";
+  }
+
+  private _paneRight(paneWidths: number[], index: number): number {
+    return paneWidths.slice(0, index+1).reduce( (accu, w) => accu+w, 0) + index * DIVIDER_WIDTH;
+  }
+
+  private _adjustPaneWidth(paneWidths: number[], dividerIndex: number, indicatorLeft: number): number[] {
+    let delta = indicatorLeft - this._paneRight(this._paneWidths, dividerIndex);
+
+    const widthLeft = paneWidths[dividerIndex];
+    const widthRight = paneWidths[dividerIndex+1];
+
+    delta = widthLeft+delta < MIN_PANE_WIDTH ? -widthLeft+MIN_PANE_WIDTH : delta;
+    delta = widthRight-delta < MIN_PANE_WIDTH ? widthRight-MIN_PANE_WIDTH : delta;
+
+    const copy = paneWidths.slice();
+    copy[dividerIndex] += delta;
+    copy[dividerIndex+1] -= delta;
     
+    return copy;
+  }
+
+
+  private _handleMouseLeave(ev: MouseEvent): void {
+    this._log.debug("_handleMouseLeave",ev.target);
+    this._stopDrag();
   }
 
   private _createLayout(): void {
@@ -238,12 +304,31 @@ export class Splitter extends ThemeableElementBase {
       }
     }
 
-    this._setSizes();
+    this._setSizes(this._paneWidths);
   }
 
-  private _setSizes(): void {
+  private _setSizes(paneWidths: number[]): void {
     const topDiv = DomUtils.getShadowId(this, ID_CONTAINER);
-    
+
+    let x = 0;
+    for (let i=0; i<paneWidths.length; i++) {
+      const kid = <HTMLElement> topDiv.children.item(i*2);
+      const width = paneWidths[i];
+      kid.style.left = "" + x + "px";
+      kid.style.width = "" + width + "px";
+      x += width;
+
+      if (i < this.children.length-1) {
+        const divider = <HTMLElement> topDiv.children.item(i*2+1);
+        divider.style.left = "" + x + "px";
+        divider.style.width = "" + DIVIDER_WIDTH + "px";
+        x += DIVIDER_WIDTH;
+      }      
+    }
+  }
+
+  private _equalPaneWidths(): number[] {
+    const topDiv = DomUtils.getShadowId(this, ID_CONTAINER);
     const rect = topDiv.getBoundingClientRect();
     const paneCount = this.children.length;
 
@@ -251,27 +336,8 @@ export class Splitter extends ThemeableElementBase {
     const paneWidths: number[] = [];
     for (let i=0; i<paneCount; i++) {
       paneWidths.push(paneWidth);
-      if (i < this.children.length-1) {
-        paneWidths.push(DIVIDER_WIDTH);
-      }
     }
-
-    let x = 0;
-    for (let i=0; i<paneCount; i++) {
-      const kid = <HTMLElement> topDiv.children.item(i*2);
-      const width = paneWidths[i*2];
-      kid.style.left = "" + x + "px";
-      kid.style.width = "" + width + "px";
-      x += width;
-
-      if (i < this.children.length-1) {
-        const divider = <HTMLElement> topDiv.children.item(i*2+1);
-        const width = paneWidths[i*2+1];
-        divider.style.left = "" + x + "px";
-        divider.style.width = "" + width + "px";
-        x += width;
-      }      
-    }
+    return paneWidths;
   }
 
 }
