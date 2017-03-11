7abd07611f18822dff8f6a0d7707a3cca29b5617
Simon Edwards <simon@simonzone.com>
2017-03-11 09:07:07 +0100

    Remove the last bits of CSS used by the old split feature

--- src/MainWebUi.ts
+++ src/MainWebUi.ts
@@ -65,14 +65,9 @@ const ID_OSX_MINIMIZE_BUTTON = "ID_OSX_MINIMIZE_BUTTON";
 const ID_OSX_MAXIMIZE_BUTTON = "ID_OSX_MAXIMIZE_BUTTON";
 const ID_OSX_CLOSE_BUTTON = "ID_OSX_CLOSE_BUTTON";
 
-const ID_PANE_LEFT = "ID_PANE_LEFT";
-const ID_PANE_RIGHT = "ID_PANE_RIGHT";
-const ID_GAP = "ID_GAP";
 const ID_TAB_CONTAINER_LEFT = "ID_TAB_CONTAINER_LEFT";
 const ID_REST_DIV_PRIMARY = "ID_REST_DIV_PRIMARY";
 const ID_REST_DIV_LEFT = "ID_REST_DIV_LEFT";
-const ID_REST_DIV_SECONDARY = "ID_REST_DIV_SECONDARY";
-const ID_LEFT_REST_DIV_SECONDARY = "ID_LEFT_REST_DIV_SECONDARY";
 const ID_NEW_TAB_BUTTON_PRIMARY = "ID_NEW_TAB_BUTTON_PRIMARY";
 
 const CLASS_SPLIT = "split";
@@ -295,17 +290,15 @@ export class MainWebUi extends ThemeableElementBase implements keybindingmanager
         `<button id="${ID_CLOSE_BUTTON}" tabindex="-1"></button>` +
       `</div>` +
       `<div id="${ID_MAIN_CONTENTS}">` +
-        `<div id="${ID_PANE_LEFT}">` +
-          `<${TabWidget.TAG_NAME} id="${ID_TAB_CONTAINER_LEFT}" show-frame="false">` +
-            `<div id="${ID_REST_DIV_LEFT}">` +
-              `<button id="${ID_OSX_CLOSE_BUTTON}" tabindex="-1"></button>` +
-              `<button id="${ID_OSX_MINIMIZE_BUTTON}" tabindex="-1"></button>` +
-              `<button id="${ID_OSX_MAXIMIZE_BUTTON}" tabindex="-1"></button>` +
-            `</div>` +
-            `<div id="${ID_REST_DIV_PRIMARY}"><button class="btn btn-quiet" id="${ID_NEW_TAB_BUTTON_PRIMARY}"><i class="fa fa-plus"></i></button>` +
-            `<slot></slot></div>` +
-          `</${TabWidget.TAG_NAME}>` +
-        `</div>` +
+        `<${TabWidget.TAG_NAME} id="${ID_TAB_CONTAINER_LEFT}" show-frame="false">` +
+          `<div id="${ID_REST_DIV_LEFT}">` +
+            `<button id="${ID_OSX_CLOSE_BUTTON}" tabindex="-1"></button>` +
+            `<button id="${ID_OSX_MINIMIZE_BUTTON}" tabindex="-1"></button>` +
+            `<button id="${ID_OSX_MAXIMIZE_BUTTON}" tabindex="-1"></button>` +
+          `</div>` +
+          `<div id="${ID_REST_DIV_PRIMARY}"><button class="btn btn-quiet" id="${ID_NEW_TAB_BUTTON_PRIMARY}"><i class="fa fa-plus"></i></button>` +
+          `<slot></slot></div>` +
+        `</${TabWidget.TAG_NAME}>` +
       `</div>` +
     `</div>`;
   }
