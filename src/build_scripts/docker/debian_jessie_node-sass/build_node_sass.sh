1eb31197e3c67b2a724911fcc57b9e897484bf47
Simon Edwards <simon@simonzone.com>
2017-03-15 23:11:39 +0100

    Fix the Electron version used to build the node-sass native module on Linux x64

--- src/build_scripts/docker/debian_jessie_node-sass/build_node_sass.sh
+++ src/build_scripts/docker/debian_jessie_node-sass/build_node_sass.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir output
-docker run -i -t --rm -e ELECTRON_VERSION=1.3.4 -e NODESASS_VERSION=v4.1.0 -v $(pwd)/output:/data:rw node-sass-build-env
+docker run -i -t --rm -e ELECTRON_VERSION=1.4.12 -e NODESASS_VERSION=v4.1.0 -v $(pwd)/output:/data:rw node-sass-build-env
