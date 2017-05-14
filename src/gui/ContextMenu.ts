2e6f8ecdd4e28916f16c2dcd1347c8082c40ca24
Simon Edwards <simon@simonzone.com>
2017-05-14 21:56:25 +0200

    Refactor the ContextMenu

--- src/gui/ContextMenu.ts
+++ src/gui/ContextMenu.ts
@@ -10,12 +10,17 @@ import {MenuItem} from './MenuItem';
 import {CheckboxMenuItem} from './CheckboxMenuItem';
 import * as DomUtils from '../DomUtils';
 import * as Util from './Util';
+import Logger from '../Logger';
 
 MenuItem.init();
 
 const ID = "EtContextMenuTemplate";
 const ID_COVER = "ID_COVER";
 const ID_CONTAINER = "ID_CONTAINER";
+const CLASS_COVER_CLOSED = "CLASS_COVER_CLOSED";
+const CLASS_COVER_OPEN = "CLASS_COVER_OPEN";
+const CLASS_CONTAINER_CLOSED = "CLASS_CONTAINER_CLOSED";
+const CLASS_CONTAINER_OPEN = "CLASS_CONTAINER_OPEN";
 
 let registered = false;
 
@@ -23,7 +28,7 @@ let registered = false;
  * A context menu.
  */
 export class ContextMenu extends ThemeableElementBase {
-  
+   
   /**
    * The HTML tag name of this element.
    */
@@ -33,7 +38,7 @@ export class ContextMenu extends ThemeableElementBase {
    * Initialize the ContextMenu class and resources.
    *
    * When ContextMenu is imported into a render process, this static method
-   * must be called before an instances may be created. This is can be safely
+   * must be called before an instances may be created. This can be safely
    * called multiple times.
    */
   static init(): void {
@@ -43,29 +48,7 @@ export class ContextMenu extends ThemeableElementBase {
     }
   }
   
-  /**
-   * 
-   */
-  private createClone() {
-    let template = <HTMLTemplateElement>window.document.getElementById(ID);
-    if (template === null) {
-      template = <HTMLTemplateElement>window.document.createElement('template');
-      template.id = ID;
-      template.innerHTML = `<style id="${ThemeableElementBase.ID_THEME}"></style>
-        <div id='${ID_COVER}' class='cover_closed'></div>
-        <div id='${ID_CONTAINER}' class='container_closed' tabindex='0'><slot></slot></div>`;
-      window.document.body.appendChild(template);
-    }
-
-    return window.document.importNode(template.content, true);
-  }
-
-  /**
-   * 
-   */
-  private __getById(id:string): Element {
-    return DomUtils.getShadowRoot(this).querySelector('#'+id);
-  }
+  private _log: Logger;
   
   protected _themeCssFiles(): ThemeTypes.CssFile[] {
     return [ThemeTypes.CssFile.GUI_CONTEXTMENU];
@@ -85,6 +68,7 @@ export class ContextMenu extends ThemeableElementBase {
 
   constructor() {
     super();
+    this._log = new Logger(ContextMenu.TAG_NAME, this);
     
     const shadow = this.attachShadow({ mode: 'open', delegatesFocus: false });
     const clone = this.createClone();
@@ -135,11 +119,24 @@ export class ContextMenu extends ThemeableElementBase {
     container.addEventListener('keypress', (ev: KeyboardEvent) => { this.handleKeyPress(ev); });
   }
 
-  //-----------------------------------------------------------------------
+  private createClone() {
+    let template = <HTMLTemplateElement>window.document.getElementById(ID);
+    if (template === null) {
+      template = <HTMLTemplateElement>window.document.createElement('template');
+      template.id = ID;
+      template.innerHTML = `<style id="${ThemeableElementBase.ID_THEME}"></style>
+          <div id='${ID_COVER}' class='${CLASS_COVER_CLOSED}'></div>
+          <div id='${ID_CONTAINER}' class='${CLASS_CONTAINER_CLOSED}' tabindex='0'><slot></slot></div>`;
+      window.document.body.appendChild(template);
+    }
 
-  /**
-   * 
-   */
+    return window.document.importNode(template.content, true);
+  }
+
+  private __getById(id:string): Element {
+    return DomUtils.getShadowRoot(this).querySelector('#'+id);
+  }
+
   private fetchMenuItems(kids: NodeList): MenuItem[] {
     const len = kids.length;
     const result: MenuItem[] = [];
@@ -153,9 +150,6 @@ export class ContextMenu extends ThemeableElementBase {
     return result;
   }
 
-  /**
-   * 
-   */
   private selectMenuItem(kids: NodeList, selectitem: Element) {
     const len = kids.length;
     for (let i=0; i<len; i++) {
@@ -167,11 +161,7 @@ export class ContextMenu extends ThemeableElementBase {
     }
   }
 
-  /**
-   * 
-   */
   private handleKeyDown(ev: KeyboardEvent) {
-    // Escape.
     if (ev.key === "Escape") {
       this.close();
       ev.preventDefault();
@@ -215,9 +205,6 @@ export class ContextMenu extends ThemeableElementBase {
     ev.stopPropagation();
   }
 
-  /**
-   * 
-   */
   private activateItem(item: MenuItem): void {
     item._clicked();
 
@@ -229,9 +216,6 @@ export class ContextMenu extends ThemeableElementBase {
     this.dispatchEvent(event);
   }
 
-  /**
-   * 
-   */
   private handleKeyPress(ev: KeyboardEvent): void {
     ev.preventDefault();
     ev.stopPropagation();
@@ -249,16 +233,13 @@ export class ContextMenu extends ThemeableElementBase {
     }  
   }
 
-  /**
-   * 
-   */
   open(x: number, y: number): void {
     // Nuke any style like 'display: none' which can be use to prevent flicker.
     this.setAttribute('style', '');
     
     const container = <HTMLDivElement>this.__getById(ID_CONTAINER);
-    container.classList.remove('container_closed');
-    container.classList.add('container_open');  
+    container.classList.remove(CLASS_CONTAINER_CLOSED);
+    container.classList.add(CLASS_CONTAINER_OPEN);
 
     const rect = container.getBoundingClientRect();
 
@@ -275,17 +256,17 @@ export class ContextMenu extends ThemeableElementBase {
     container.style.left = "" + sx + "px";
     container.style.top = "" + sy + "px";
 
-    const cover = <HTMLDivElement>this.__getById(ID_COVER);
-    cover.className = "cover_open";
-
+    this._openCover();
     this.selectMenuItem(this.childNodes, null);
 
     container.focus();
   }
 
-  /**
-   * 
-   */
+  private _openCover(): void {
+    const cover = <HTMLDivElement> DomUtils.getShadowId(this, ID_COVER);
+    cover.className = CLASS_COVER_OPEN;
+  }
+
   private debugScroll(msg?: string) {
     const text = msg !== undefined ? msg : "";
     const termdiv = window.document.querySelector('div.terminal');
@@ -299,56 +280,54 @@ export class ContextMenu extends ThemeableElementBase {
     }
   }
 
-  /**
-   * 
-   */
-  openAround(el: HTMLElement) {
+  openAround(targetElement: HTMLElement) {
     // Nuke any style like 'display: none' which can be use to prevent flicker.
     this.setAttribute('style', '');
     
-    const elrect = el.getBoundingClientRect();
-
+    const targetElementRect = targetElement.getBoundingClientRect();
     const container = <HTMLDivElement>this.__getById(ID_CONTAINER);
-    container.classList.remove('container_closed');  
-    container.classList.add('container_open');  
-    const containerrect = container.getBoundingClientRect();
+    container.classList.remove(CLASS_CONTAINER_CLOSED);
+    container.classList.add(CLASS_CONTAINER_OPEN);  
+    const containerRect = container.getBoundingClientRect();
 
-    let sx = elrect.left;
-    if (sx+containerrect.width > window.innerWidth) {
-      sx = window.innerWidth - containerrect.width;
+    let containerX = targetElementRect.left;
+    if (containerX + containerRect.width > window.innerWidth) {
+      containerX = window.innerWidth - containerRect.width;
     }
 
-    let sy = elrect.bottom;
-    if (sy+containerrect.height > window.innerHeight) {
-      sy = elrect.top - containerrect.height;
+    let containerY = targetElementRect.bottom;
+    if (containerY+containerRect.height > window.innerHeight) {
+      containerY = targetElementRect.top - containerRect.height;
     }
 
-    container.style.left = "" + sx + "px";
-    container.style.top = "" + sy + "px";
+    container.style.left = "" + containerX + "px";
+    container.style.top = "" + containerY + "px";
 
-    const cover = <HTMLDivElement>this.__getById(ID_COVER);
-    cover.className = "cover_open";
-
+    this._openCover();
     this.selectMenuItem(this.childNodes, null);
-
     container.focus();
   }
 
-  /**
-   * 
-   */
   close(): void {
     let event = new CustomEvent('before-close', { detail: null });
     this.dispatchEvent(event);
 
     const cover = <HTMLDivElement>this.__getById(ID_COVER);
-    cover.className = "cover_closed";
+    cover.className = CLASS_COVER_CLOSED;
 
     const container = <HTMLDivElement>this.__getById(ID_CONTAINER);
-    container.classList.remove('container_open');  
-    container.classList.add('container_closed');
+    container.classList.remove(CLASS_CONTAINER_OPEN);
+    container.classList.add(CLASS_CONTAINER_CLOSED);
 
     event = new CustomEvent('close', { detail: null });
     this.dispatchEvent(event);
   }
+
+//   private _viewportCoordsToRelative(x: number, y: number): {x: number, y: number} {
+//     const topContainer = DomUtils.getShadowId(this, ID_TOP_CONTAINER);
+//     const topRect = topContainer.getBoundingClientRect();
+// this._log.debug("topRect: ",topRect);
+//     return {x: x - topRect.left, y: y - topRect.bottom};
+//   }
+
 }
