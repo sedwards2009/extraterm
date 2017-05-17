e34658b3bfc29304e03123b458f0a7b3b70158ed
Simon Edwards <simon@simonzone.com>
2017-05-17 22:12:07 +0200

    Bump node-sass up to version 4.5.3 and add Linux x64 binary

--- src/build_scripts/docker/debian_jessie_node-sass/build_node_sass.sh
+++ src/build_scripts/docker/debian_jessie_node-sass/build_node_sass.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir output
-docker run -i -t --rm -e ELECTRON_VERSION=1.6.6 -e NODESASS_VERSION=v4.1.0 -v $(pwd)/output:/data:rw node-sass-build-env
+docker run -i -t --rm -e ELECTRON_VERSION=1.6.8 -e NODESASS_VERSION=v4.5.3 -v $(pwd)/output:/data:rw node-sass-build-env
