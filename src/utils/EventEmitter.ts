371abc1ccc46e5f4c14927172e08e5d2988ff5ed
Simon Edwards <simon@simonzone.com>
2017-10-13 16:15:50 +0200

    Docs for the EventEmitter class

--- src/utils/EventEmitter.ts
+++ src/utils/EventEmitter.ts
@@ -6,12 +6,19 @@
 
 import {Disposable, Event} from 'extraterm-extension-api';
 
-// FIXME docs
-
+/**
+ * An event emitter which can be subscribe to to hear when this event is fired.
+ */
 export class EventEmitter<T> {
 
   private _listeners: ((t: T) => void)[] = [];
 
+  /**
+   * Attach a listener to this event.
+   * 
+   * @param listener the function to call when this event is triggered.
+   * @return a `Disposible` which when used disconnects this listener from the event.
+   */
   event: Event<T> = (listener: (t: T) => void): Disposable => {
     this._listeners.push(listener);
     return {
@@ -21,6 +28,11 @@ export class EventEmitter<T> {
     };
   }
 
+  /**
+   * Fire the event to all listeners.
+   * 
+   * @param t the payload of the event.
+   */
   fire(t: T): void {
     this._listeners.forEach(listener => listener(t));
   }
