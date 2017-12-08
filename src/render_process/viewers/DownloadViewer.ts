809a53f785232c229385aebe416f9897056af7e9
Simon Edwards <simon@simonzone.com>
2017-12-08 22:12:11 +0100

    Monitor the bulk file handle and show the download progress bytes in the Download viewer.
    issue #67

--- src/render_process/viewers/DownloadViewer.ts
+++ src/render_process/viewers/DownloadViewer.ts
@@ -2,6 +2,7 @@
  * Copyright 2017 Simon Edwards <simon@simonzone.com>
  */
 
+import { Disposable } from 'extraterm-extension-api';
 import {WebComponent} from 'extraterm-web-component-decorators';
 import Vue from 'vue';
 import Component from 'vue-class-component';
@@ -12,16 +13,51 @@ import log from '../../logging/LogDecorator';
 import {SimpleViewerElement} from '../viewers/SimpleViewerElement';
 
 
-const DEBUG_SIZE = false;
-
-export const ID_CONTAINER = "ID_CONTAINER";
-
 @Component(
 {
-  template: `<span>Download viewer</span>`
+  template: `<div class="body container-fluid"">
+  <div class="row">
+    <div v-if="!finished" class="col-sm-6">{{name}}</div>
+    <div v-if="finished" class="col-sm-6">Downloading {{name}}</div>
+    <div class="col-sm-6">{{formattedHumanAvailableBytes}} ({{formattedExactAvailableBytes}} bytes)</div>
+  </div>
+</div>`
 })
 class DownloadUI extends Vue {
+  name: string = "-";
+  totalSize: number = 0;
+  availableSize: number = 0;
+  finished: boolean = false;
 
+  get formattedExactAvailableBytes(): string {
+    return this.availableSize.toLocaleString("en-US");
+  }
+
+  get formattedHumanAvailableBytes(): string {
+    return formatHumanBytes(this.availableSize);
+  }
+}
+
+function formatHumanBytes(numberBytes: number): string {
+  const kibibytes = numberBytes / 1024;
+  const mebibytes = numberBytes / (1024 * 1024);
+  const gibibytes = numberBytes / (1024 * 1024 * 1024);
+  let displayNumber = 0;
+  let units = "";
+  if (gibibytes > 1) {
+    displayNumber = gibibytes;
+    units = "GiB";
+  } else if (mebibytes > 1) {
+    displayNumber = mebibytes;
+    units = "MiB";
+  } else if (kibibytes > 1) {
+    displayNumber = kibibytes;
+    units = "KiB";
+  } else {
+    displayNumber = numberBytes;
+    units = "b";
+  }
+  return displayNumber.toLocaleString("en-US", {maximumFractionDigits: 1}) + units;
 }
 
 @WebComponent({tag: "et-download-viewer"})
@@ -30,8 +66,10 @@ export class DownloadViewer extends SimpleViewerElement {
   static TAG_NAME = "et-download-viewer";
 
   private _log: Logger;
-  private _bulkFileHandle: BulkFileHandle;
+  private _bulkFileHandle: BulkFileHandle = null;
   private _ui: DownloadUI = null;
+  private _onAvailableSizeChangeDisposable: Disposable = null;
+  private _onFinishedDisposable: Disposable = null;
 
   constructor() {
     super();
@@ -60,6 +98,37 @@ export class DownloadViewer extends SimpleViewerElement {
   }
 
   setBulkFileHandle(handle: BulkFileHandle): void {
-    // this._loadBulkFile(handle);
+    this._releaseBulkFileHandle();
+
+    this._bulkFileHandle = handle;
+    handle.ref();
+
+    this._onAvailableSizeChangeDisposable = this._bulkFileHandle.onAvailableSizeChange(() => {
+        this._ui.availableSize = handle.getAvailableSize();
+      });
+    this._onFinishedDisposable = this._bulkFileHandle.onFinished(() => {
+      this._ui.finished = true;
+    });
+    this._ui.availableSize = handle.getAvailableSize();
+    this._ui.totalSize = handle.getTotalSize()
+
+    const metadata = handle.getMetadata();
+    if (metadata["filename"] !== undefined) {
+      this._ui.name = <string> metadata["filename"];
+    }
+  }
+
+  private _releaseBulkFileHandle(): void {    
+    if (this._bulkFileHandle !== null) {
+      this._onAvailableSizeChangeDisposable.dispose();
+      this._onFinishedDisposable.dispose();
+      this._bulkFileHandle.deref();
+    }
+    this._bulkFileHandle = null;
+  }
+
+  dispose(): void {
+    this._releaseBulkFileHandle();
+    super.dispose();
   }
 }
