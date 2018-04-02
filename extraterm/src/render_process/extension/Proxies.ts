3fb3349d549a4913e758927bc8c1a68e6375f649
Simon Edwards <simon@simonzone.com>
2018-04-02 14:04:16 +0200

    Refactor WorkspaceProxy by splitting out the viewer registration code

--- extraterm/src/render_process/extension/Proxies.ts
+++ extraterm/src/render_process/extension/Proxies.ts
@@ -10,18 +10,11 @@ import {DisposableItemList} from '../../utils/DisposableUtils';
 import {EtTerminal, EXTRATERM_COOKIE_ENV} from '../Terminal';
 import {ExtensionUiUtils, InternalExtensionContext, InternalWorkspace, ProxyFactory} from './InternalTypes';
 import {Logger, getLogger} from '../../logging/Logger';
-import { SimpleViewerElement } from '../viewers/SimpleViewerElement';
-import { ViewerElement } from '../viewers/ViewerElement';
-import { ExtensionViewerContribution, ExtensionSessionEditorContribution } from '../../ExtensionMetadata';
-import { CssFile } from '../../theme/Theme';
+import { ExtensionSessionEditorContribution } from '../../ExtensionMetadata';
 import { ThemeableElementBase } from '../ThemeableElementBase';
 import { WorkspaceCommandsRegistry } from './WorkspaceCommandsRegistry';
+import { WorkspaceViewerRegistry, ExtensionViewerBaseImpl } from './WorkspaceViewerRegistry';
 
-
-interface RegisteredViewer {
-  tag: string;
-  mimeTypes: string[];
-}
 
 interface RegisteredSessionEditor {
   type: string;
@@ -32,12 +25,15 @@ interface RegisteredSessionEditor {
 export class WorkspaceProxy implements InternalWorkspace {
   private _log: Logger = null;
   private _workspaceCommandsRegistry: WorkspaceCommandsRegistry = null;
-  private _registeredViewers: RegisteredViewer[] = [];
+  private _workspaceViewerRegistry: WorkspaceViewerRegistry = null;
+
   private _registeredSessionEditors: RegisteredSessionEditor[] = [];
 
   constructor(private _internalExtensionContext: InternalExtensionContext) {
     this._log = getLogger("WorkspaceProxy", this);
     this._workspaceCommandsRegistry = new WorkspaceCommandsRegistry();
+    this._workspaceViewerRegistry = new WorkspaceViewerRegistry(this._internalExtensionContext);
+    
     this.extensionViewerBaseConstructor = ExtensionViewerBaseImpl;
     this.extensionSessionEditorBaseConstructor = ExtensionSessionEditorBaseImpl;    
   }
@@ -79,54 +75,11 @@ export class WorkspaceProxy implements InternalWorkspace {
   extensionViewerBaseConstructor: ExtensionApi.ExtensionViewerBaseConstructor;
 
   registerViewer(name: string, viewerClass: ExtensionApi.ExtensionViewerBaseConstructor): void {
-    let viewerMetadata: ExtensionViewerContribution = null;
-    for (const vmd of this._internalExtensionContext.extensionMetadata.contributions.viewer) {
-      if (vmd.name === name) {
-        viewerMetadata = vmd;
-        break;
-      }
-    }
-
-    if (viewerMetadata == null) {
-      this._log.warn(`Unable to register viewer '${name}' for extension ` +
-        `'${this._internalExtensionContext.extensionMetadata.name}' because the viewer contribution data couldn't ` +
-        `be found in the extension's package.json file.`);
-      return;
-    }
-
-    const internalExtensionContext = this._internalExtensionContext;
-
-    const viewerElementProxyClass = class extends ExtensionViewerProxy {
-      protected _createExtensionViewer(): ExtensionApi.ExtensionViewerBase {
-        return new viewerClass(this);
-      }
-
-      protected _getExtensionContext(): InternalExtensionContext {
-        return internalExtensionContext;
-      }
-    
-      protected _getExtensionViewerContribution(): ExtensionViewerContribution {
-        return viewerMetadata;
-      }
-    };
-    
-// FIXME
-    const tag = this._internalExtensionContext.extensionMetadata.name + "-" + kebabCase(name);
-    this._log.info("Registering custom element ", tag);
-    window.customElements.define(tag, viewerElementProxyClass);
-
-    this._registeredViewers.push({
-      mimeTypes: viewerMetadata.mimeTypes, tag
-    });
+    this._workspaceViewerRegistry.registerViewer(name, viewerClass);
   }
-
+   
   findViewerElementTagByMimeType(mimeType: string): string {
-    for (const registeredViewer of this._registeredViewers) {
-      if (registeredViewer.mimeTypes.indexOf(mimeType) !== -1) {
-        return registeredViewer.tag;
-      }
-    }
-    return null;
+    return this._workspaceViewerRegistry.findViewerElementTagByMimeType(mimeType);
   }
 
   extensionSessionEditorBaseConstructor: ExtensionApi.ExtensionSessionEditorBaseConstructor;
@@ -179,104 +132,6 @@ function kebabCase(name: string): string {
   return name.split(/(?=[ABCDEFGHIJKLMNOPQRSTUVWXYZ])/g).map(s => s.toLowerCase()).join("-");
 }
 
-
-class ExtensionViewerProxy extends SimpleViewerElement {
-  private _extensionViewer: ExtensionApi.ExtensionViewerBase = null;
-
-  constructor() {
-    super();
-    this._extensionViewer = this._createExtensionViewer();
-    this._extensionViewer.created();
-  }
-
-  protected _createExtensionViewer(): ExtensionApi.ExtensionViewerBase {
-    return null;
-  }
-
-  protected _getExtensionContext(): InternalExtensionContext {
-    return null;
-  }
-
-  protected _getExtensionViewerContribution(): ExtensionViewerContribution {
-    return null;
-  }
-
-  protected _themeCssFiles(): CssFile[] {
-    const extensionContext = this._getExtensionContext();
-    const name = extensionContext.extensionMetadata.name
-    const cssDecl = this._getExtensionViewerContribution().css;
-    const cssFiles = cssDecl.cssFile.map(cf =>  name + ":" + cf);
-
-    const fontAwesomeCss = cssDecl.fontAwesome ? [CssFile.FONT_AWESOME] : [];
-    return [CssFile.GUI_CONTROLS, ...fontAwesomeCss, ...cssFiles];
-  }
-
-  getMetadata(): ExtensionApi.ViewerMetadata {
-    return this._extensionViewer.getMetadata();
-  }
-  
-  _metadataUpdated(): void {
-    const event = new CustomEvent(ViewerElement.EVENT_METADATA_CHANGE, { bubbles: true });
-    this.dispatchEvent(event);
-  }
-
-  getBulkFileHandle(): ExtensionApi.BulkFileHandle {
-    return this._extensionViewer.getBulkFileHandle();
-  }
-
-  setBulkFileHandle(handle: ExtensionApi.BulkFileHandle): void {
-    this._extensionViewer.setBulkFileHandle(handle);
-  }
-}
-
-
-class ExtensionViewerBaseImpl implements ExtensionApi.ExtensionViewerBase {
-
-  private __ExtensionViewerBaseImpl_metadata: ExtensionApi.ViewerMetadata = null;
-
-  constructor(private _viewerProxy: ExtensionViewerProxy, ..._: any[]) {
-    this.__ExtensionViewerBaseImpl_metadata = {
-      title: "ExtensionViewer",
-      deleteable: true,
-      moveable: true,
-      icon: null,
-      posture: ExtensionApi.ViewerPosture.NEUTRAL,
-      toolTip: null
-    };
-  }
-
-  created(): void {
-  }
-
-  getContainerElement(): HTMLElement {
-    return this._viewerProxy.getContainerNode();
-  }
-
-  getMetadata(): ExtensionApi.ViewerMetadata {
-    return this.__ExtensionViewerBaseImpl_metadata;
-  }
-  
-  updateMetadata(changes: ExtensionApi.ViewerMetadataChange): void {
-    let changed = false;
-    for (const key of Object.getOwnPropertyNames(changes)) {
-      if (this.__ExtensionViewerBaseImpl_metadata[key] !== changes[key]) {
-        this.__ExtensionViewerBaseImpl_metadata[key] = changes[key];
-        changed = true;
-      }
-    }
-
-    if (changed) {
-      this._viewerProxy._metadataUpdated();
-    }
-  }
-
-  getBulkFileHandle(): ExtensionApi.BulkFileHandle {
-    return null;
-  }
-
-  setBulkFileHandle(handle: ExtensionApi.BulkFileHandle): void {
-  }
-}
 
 class ExtensionSessionEditorProxy extends ThemeableElementBase  {
 
