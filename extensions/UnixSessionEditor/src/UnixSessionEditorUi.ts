1f2f7ce7bc41f2e01f4aeeed1ff537c478290998
Simon Edwards <simon@simonzone.com>
2018-04-20 16:02:51 +0200

    Clean out a little bit of dead code from the settings.

--- extensions/UnixSessionEditor/src/UnixSessionEditorUi.ts
+++ extensions/UnixSessionEditor/src/UnixSessionEditorUi.ts
@@ -20,14 +20,14 @@ import Vue from 'vue';
   <div class="form-group">
     <label class="col-sm-4 control-label">Shell:</label>
     <div class="input-group col-sm-8">
-      <input class="input-radio" type="radio" name="default_shell" value="1" v-model.number="useDefaultShell">
+      <input class="input-radio" type="radio" value="1" v-model.number="useDefaultShell">
       <div class="inline-text">Default login shell</div>
     </div>
   </div>
   <div class="form-group">
     <div class="col-sm-4 control-label"></div>
     <div class="input-group col-sm-8 form-inline">
-      <input class="input-radio" type="radio" name="default_shell" value="0" v-model.number="useDefaultShell">
+      <input class="input-radio" type="radio" value="0" v-model.number="useDefaultShell">
       <div class="inline-text">Other</div>
       <input id="other_shell" type="text" class="form-control" :disabled="useDefaultShell===1" v-model.lazy="shell">
     </div>
