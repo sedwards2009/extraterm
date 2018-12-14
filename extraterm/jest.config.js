9d435ec5d2cc4ed5c6dcf1f87913b5f647b57c24
Simon Edwards <simon@simonzone.com>
2018-12-14 09:47:44 +0100

    Convert PackageFileParser test over to user Jest.

--- extraterm/jest.config.js
+++ extraterm/jest.config.js
@@ -137,7 +137,7 @@ module.exports = {
 
   // The glob patterns Jest uses to detect test files
   testMatch: [
-    "**/*.jest.(ts|tsx|js)"
+    "**/*.jest.(ts|tsx)"
   ],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
---
+++ extraterm/src/main_process/extension/PackageFileParser.jest.ts
@@ -0,0 +1,67 @@
+/*
+ * Copyright 2018 Simon Edwards <simon@simonzone.com>
+ *
+ * This source code is licensed under the MIT license which is detailed in the LICENSE.txt file.
+ */
+import "jest";
+import { parsePackageJson } from './PackageFileParser';
+
+test("metadata", () => {
+  const parsed = parsePackageJson({
+    name: "Foo", version: "1.0.0", description: "Foobar"
+  }, "");
+  expect(parsed.name).toBe("Foo");
+  expect(parsed.version).toBe("1.0.0");
+  expect(parsed.description).toBe("Foobar");
+  expect(parsed.contributions.viewer.length).toBe(0);
+});
+
+test("contributions", () => {
+  const parsed = parsePackageJson({
+    name: "Foo",
+    version: "1.0.0",
+    description: "Foobar",
+    contributions: {
+      viewer: [{name: "SmegViewer", mimeTypes: ["foo/bar"]}]
+    }
+  }, "");
+
+  expect(parsed.name).toBe("Foo");
+  expect(parsed.version).toBe("1.0.0");
+  expect(parsed.description).toBe("Foobar");
+  expect(parsed.contributions.viewer.length).toBe(1);
+  expect(parsed.contributions.viewer[0].name).toBe("SmegViewer");
+  expect(parsed.contributions.viewer[0].mimeTypes.length).toBe(1);
+  expect(parsed.contributions.viewer[0].mimeTypes[0]).toBe("foo/bar");
+});
+
+test("platform", () => {
+  const parsed = parsePackageJson({
+    name: "Foo",
+    version: "1.0.0",
+    description: "Foobar",
+    includePlatform: [
+      {
+        os: "linux",
+        arch: "x64"
+      }
+    ],
+    excludePlatform: [
+      {
+        os: "macos"
+      },
+      {
+        arch: "mips"
+      }
+    ]
+  }, "");
+
+  expect(parsed.includePlatform.length).toBe(1);
+  expect(parsed.includePlatform[0].os).toBe("linux");
+  expect(parsed.includePlatform[0].arch).toBe("x64");
+  expect(parsed.excludePlatform.length).toBe(2);
+  expect(parsed.excludePlatform[0].os).toBe("macos");
+  expect(parsed.excludePlatform[0].arch).toBe(null);
+  expect(parsed.excludePlatform[1].arch).toBe("mips");
+});
+
