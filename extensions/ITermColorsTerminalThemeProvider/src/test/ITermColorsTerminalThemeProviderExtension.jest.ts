4fd34a1364daade7a0ec8108711caa0f2abde9dc
Simon Edwards <simon@simonzone.com>
2018-12-24 18:37:35 +0100

    Convert ITermColorsTerminalThemeProvider to using Jest instead of nodeunit.

--- extensions/ITermColorsTerminalThemeProvider/src/test/ITermColorsTerminalThemeProviderExtension.jest.ts
+++ extensions/ITermColorsTerminalThemeProvider/src/test/ITermColorsTerminalThemeProviderExtension.jest.ts
@@ -3,36 +3,31 @@
  *
  * This source code is licensed under the MIT license which is detailed in the LICENSE.txt file.
  */
-import * as nodeunit from "nodeunit";
+import "jest";
 import * as path from "path";
-
 import { getLogger } from "extraterm-logging";
-
 import { ItermColorTerminalThemeProvider } from "../ITermColorsTerminalThemeProviderExtension";
 
 
 const themePaths = [path.join(__dirname, "..", "..", "src", "test")];
 
-export function testScan(test: nodeunit.Test): void {
+test("scan", () => {
   const logger = getLogger("TextMateSyntaxThemeProvider");
 
   const provider = new ItermColorTerminalThemeProvider(logger);
   const themeList = provider.scanThemes(themePaths);
 
-  test.equal(themeList.length, 1);
-  test.equal(themeList[0].name, "Dracula");
-  test.done();
-}
+  expect(themeList.length).toBe(1);
+  expect(themeList[0].name).toBe("Dracula");
+});
 
-export function testRead(test: nodeunit.Test): void {
+test("read", () => {
   const logger = getLogger("TextMateSyntaxThemeProvider");
 
   const provider = new ItermColorTerminalThemeProvider(logger);
   const themeContents = provider.readTheme(themePaths, "Dracula.itermcolors");
 
-  test.equal(themeContents[0], "#000000");
-  test.equal(themeContents[1], "#ff5555");
-	test.equal(themeContents.foregroundColor, "#f8f8f2");
- 
-  test.done();
-}
+  expect(themeContents[0]).toBe("#000000");
+  expect(themeContents[1]).toBe("#ff5555");
+	expect(themeContents.foregroundColor).toBe("#f8f8f2");
+});
