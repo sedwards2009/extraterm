c54616a042a6d414a4e4f07c020433894e0af6fe
Simon Edwards <simon@simonzone.com>
2019-02-15 15:43:10 +0100

    Update native modules for packaging. (Linux)

--- build_scripts/node_modules-linux-x64/node-pty/lib/windowsPtyAgent.test.js
+++ build_scripts/node_modules-linux-x64/node-pty/lib/windowsPtyAgent.test.js
@@ -1,6 +1,7 @@
 "use strict";
 /**
  * Copyright (c) 2017, Daniel Imms (MIT License).
+ * Copyright (c) 2018, Microsoft Corporation (MIT License).
  */
 Object.defineProperty(exports, "__esModule", { value: true });
 var assert = require("assert");
@@ -43,9 +44,6 @@ if (process.platform === 'win32') {
             it('doesn\'t escape multiple backslashes', function () {
                 check('asdf \\\\qwer', [], '"asdf \\\\qwer"');
             });
-            it('adds backslashes before quotes', function () {
-                check('"asdf "qwer"', [], '"\\"asdf \\"qwer\\""');
-            });
             it('escapes backslashes before quotes', function () {
                 check('asdf \\"qwer', [], '"asdf \\\\\\"qwer"');
             });
@@ -57,6 +55,18 @@ if (process.platform === 'win32') {
             it('joins arguments with spaces', function () {
                 check('asdf', ['qwer zxcv', '', '"'], 'asdf "qwer zxcv" "" \\"');
             });
+            it('array argument all in quotes', function () {
+                check('asdf', ['"surounded by quotes"'], 'asdf \\"surounded by quotes\\"');
+            });
+            it('array argument quotes in the middle', function () {
+                check('asdf', ['quotes "in the" middle'], 'asdf "quotes \\"in the\\" middle"');
+            });
+            it('array argument quotes near start', function () {
+                check('asdf', ['"quotes" near start'], 'asdf "\\"quotes\\" near start"');
+            });
+            it('array argument quotes near end', function () {
+                check('asdf', ['quotes "near end"'], 'asdf "quotes \\"near end\\""');
+            });
         });
         describe('Args as CommandLine', function () {
             it('should handle empty string', function () {
@@ -65,6 +75,14 @@ if (process.platform === 'win32') {
             it('should not change args', function () {
                 check('file', 'foo bar baz', 'file foo bar baz');
                 check('file', 'foo \\ba"r \baz', 'file foo \\ba"r \baz');
+            });
+        });
+        describe('Real-world cases', function () {
+            it('quotes within quotes', function () {
+                check('cmd.exe', ['/c', 'powershell -noexit -command \'Set-location \"C:\\user\"\''], 'cmd.exe /c "powershell -noexit -command \'Set-location \\\"C:\\user\\"\'"');
+            });
+            it('space within quotes', function () {
+                check('cmd.exe', ['/k', '"C:\\Users\\alros\\Desktop\\test script.bat"'], 'cmd.exe /k \\"C:\\Users\\alros\\Desktop\\test script.bat\\"');
             });
         });
     });
