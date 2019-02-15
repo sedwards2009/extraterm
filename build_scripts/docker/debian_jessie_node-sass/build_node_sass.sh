2601d961a874e12fc7118c3499195d9b09390f7a
Simon Edwards <simon@simonzone.com>
2019-02-15 16:49:53 +0100

    Update node-sass for Linux.

--- build_scripts/docker/debian_jessie_node-sass/build_node_sass.sh
+++ build_scripts/docker/debian_jessie_node-sass/build_node_sass.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir output
-docker run -i -t --rm -e ELECTRON_VERSION=2.0.0 -e NODESASS_VERSION=v4.9.0 -v $(pwd)/output:/data:rw node-sass-build-env
+docker run -i -t --rm -e ELECTRON_VERSION=4.0.4 -e NODESASS_VERSION=v4.9.0 -v $(pwd)/output:/data:rw node-sass-build-env
