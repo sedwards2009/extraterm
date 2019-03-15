7d60ea95755e7390a10d0178f01dfbe20a72f3ff
Simon Edwards <simon@simonzone.com>
2019-03-15 13:31:22 +0100

    Update the old build_packages.js script to use the new packaging functions.

--- build_scripts/build_packages.js
+++ build_scripts/build_packages.js
@@ -6,11 +6,10 @@
 require('shelljs/global');
 const fs = require('fs');
 const path = require('path');
-const packager = require('electron-packager');
 const getRepoInfo = require('git-repo-info');
+const makePackage = require('./packaging_functions').makePackage;
 
 const log = console.log.bind(console);
-const MODULE_VERSON = 69; // This version number also appears in thememanager.ts
 
 async function main() {
   "use strict";
@@ -53,210 +52,17 @@ async function main() {
   echo("Removing development dependencies");
   exec("yarn install --production=true");
 
+  const version = packageData.version;
+
   // Create the commands zip
   echo("Creating commands.zip");
-  const commandsDir = packageData.name + "-commands-" + packageData.version;
+  const commandsDir = packageData.name + "-commands-" + version;
   cp("-r", "extraterm/src/commands", path.join(BUILD_TMP_DIR, commandsDir));
   cd(BUILD_TMP_DIR);
   exec(`zip -y -r ${commandsDir}.zip ${commandsDir}`);
   cd(SRC_DIR);
 
   const electronVersion = packageData.devDependencies['electron'];
-
-  const ignoreRegExp = [
-    /^\/build_scripts\b/,
-    /^\/extraterm-web-component-decorators\b/,
-    /^\/extraterm-extension-api\b/,
-    /^\/test\b/,
-    /^\/build_tmp\b/,
-    /^\/src\/typedocs\b/,
-    /\.ts$/,
-    /\.js\.map$/,
-    /^\/\.git\//,
-    /^\/docs\b/,
-    /^\/resources\/extra_icons\b/,
-    /^\/src\/test\b/,
-    /^\/src\/testfiles\b/
-  ];
-
-  const ignoreFunc = function ignoreFunc(filePath) {
-    const result = ignoreRegExp.some( (exp) => exp.test(filePath));
-    return result;
-  };
-
-  function appDir(platform) {
-    return platform === "darwin" ? "Extraterm.app/Contents/Resources/app" : "resources/app";
-  }
-
-  function pruneNodeSass(versionedOutputDir, arch, platform) {
-    const gutsDir = appDir(platform);
-    const nodeSassVendorDir = path.join(versionedOutputDir, gutsDir, "node_modules/node-sass/vendor");
-
-    rm('-rf', nodeSassVendorDir);
-    
-    const nodeSassBinaryDir = path.join(versionedOutputDir, gutsDir, "src/node-sass-binary");
-    ["darwin-x64", "linux-ia32", "linux-x64", "win32-x64"].forEach( (name) => {
-      if (name !== platform + "-" + arch) {
-        rm('-rf', path.join(nodeSassBinaryDir, name + "-" + MODULE_VERSON));
-      }
-    });
-  }
-
-  function pruneEmojiOne(versionedOutputDir, platform) {
-    if (platform !== "linux") {
-      const emojiOnePath = path.join(versionedOutputDir, appDir(platform), "extraterm/resources/themes/default/emojione-android.ttf");
-      rm(emojiOnePath);
-    }
-  }
-
-  function hoistSubprojectsModules(versionedOutputDir, platform) {
-    const modulesDir = path.join(versionedOutputDir, appDir(platform), "node_modules");
-
-    // Delete the symlinks.
-    for (const item of ls(modulesDir)) {
-      const itemPath = path.join(modulesDir, item);
-      if (test('-L', itemPath)) {
-        echo(`Deleting symlink ${item} in ${modulesDir}`);
-        rm(itemPath);
-      }
-    }
-
-    // Move the 'packages' subprojects up into this node_modules dir.
-    const packagesDir = path.join(versionedOutputDir, appDir(platform), "packages");
-    for (const item of ls(packagesDir)) {
-      const destDir = path.join(modulesDir, item);
-      echo(`Moving ${item} in to ${destDir}`);
-      mv(path.join(packagesDir, item), destDir);
-    }
-  }
-
-  function pruneNodeModules(versionedOutputDir, platform) {
-    const prevDir = pwd();
-    
-    cd(path.join(versionedOutputDir, appDir(platform)));
-    exec("modclean -n default:safe -r");
-    pruneSpecificNodeModules();
-
-    cd(prevDir);
-  }
-
-  function pruneSpecificNodeModules() {
-    [
-      "node-sass/src",
-      "node-sass/node_modules/nan",
-      "node-sass/vendor",
-      "node-gyp",
-      "ajv",
-      "globule",
-      "vue/src",
-      "vue/dist/vue.esm.browser.js",
-      "vue/dist/vue.esm.js",
-      "vue/dist/vue.js",
-      "vue/dist/vue.min.js",
-      "vue/dist/vue.runtime.esm.js",
-      "vue/dist/vue.runtime.js",
-      "vue/dist/vue.runtime.min.js",
-      "font-manager/src",
-      ".bin"
-    ].forEach( (subpath) => {
-      const fullPath = path.join("node_modules", subpath);
-
-      echo("Deleting " + fullPath);
-
-      if (test('-d', fullPath)) {
-        rm('-rf', fullPath);
-      } else if (test('-f', fullPath)) {
-          rm(fullPath);
-      } else {
-        echo("----------- Unable to find path "+ fullPath);
-      }
-    });
-
-  }
-
-  async function makePackage(arch, platform) {
-    log("");
-      
-    // Clean up the output dirs and files first.
-    const versionedOutputDir = packageData.name + "-" + packageData.version + "-" + platform + "-" + arch;
-    if (test('-d', versionedOutputDir)) {
-      rm('-rf', versionedOutputDir);
-    }
-    
-    const outputZip = path.join(BUILD_TMP_DIR, versionedOutputDir + ".zip");
-
-    const packagerOptions = {
-      arch: arch,
-      dir: ".",
-      platform: platform,
-      version: electronVersion,
-      ignore: ignoreFunc,
-      name: platform === "darwin" ? "Extraterm" : "extraterm",
-      overwrite: true,
-      out: BUILD_TMP_DIR,
-      prune: false
-    };
-    if (platform === "win32") {
-      packagerOptions.icon = "extraterm/resources/logo/extraterm_small_logo.ico";
-      packagerOptions.win32metadata = {
-        FileDescription: "Extraterm",
-        ProductName: "Extraterm",
-        LegalCopyright: "(C) 2018 Simon Edwards"
-      };
-    } else if (platform === "darwin") {
-      packagerOptions.icon = "extraterm/resources/logo/extraterm_small_logo.icns";
-    }
-
-    const appPath = await packager(packagerOptions);
-
-    // Rename the output dir to a one with a version number in it.
-    mv(appPath[0], path.join(BUILD_TMP_DIR, versionedOutputDir));
-    
-    const subPath = platform === "darwin"
-                      ? "Extraterm.app/Contents/Resources/app/node_modules"
-                      : "resources/app/node_modules";
-    const dirsDest = path.join(BUILD_TMP_DIR, versionedOutputDir, subPath);
-    const dirsSource = path.join("" + SRC_DIR,`build_scripts/node_modules-${platform}-${arch}`);
-    replaceDirs(dirsDest, dirsSource);
-
-    const thisCD = pwd();
-    cd(BUILD_TMP_DIR);
-
-    hoistSubprojectsModules(versionedOutputDir, platform);
-    pruneNodeModules(versionedOutputDir, platform);
-
-    // Prune any unneeded node-sass binaries.
-    pruneNodeSass(versionedOutputDir, arch, platform);
-
-    pruneEmojiOne(versionedOutputDir, platform);
-
-    // Zip it up.
-    log("Zipping up the package");
-
-    mv(path.join(versionedOutputDir, "LICENSE"), path.join(versionedOutputDir, "LICENSE_electron.txt"));
-    cp("extraterm/README.md", versionedOutputDir);
-    cp("extraterm/LICENSE.txt", versionedOutputDir);
-    
-    exec(`zip -y -r ${outputZip} ${versionedOutputDir}`);
-    cd(thisCD);
-    
-    log("App bundle written to " + versionedOutputDir);
-    return true;
-  }
-  
-  function replaceDirs(targetDir, replacementsDir) {
-    const prevDir = pwd();
-    cd(ROOT_SRC_DIR);
-    const replacements = ls(replacementsDir);
-    replacements.forEach( (rDir) => {
-      const targetSubDir = path.join(targetDir, rDir);
-      if (test('-d', targetSubDir)) {
-        rm('-r', targetSubDir);
-      }
-      cp('-r', path.join(replacementsDir, rDir), targetSubDir);
-    });  
-    cd(prevDir);
-  }
 
   function makeDmg() {
     echo("");
@@ -382,18 +188,46 @@ SectionEnd
   }
 
   if (linuxZipOnly) {
-    await makePackage("x64", "linux");
+    await makePackage( {
+      arch: "x64",
+      platform: "linux",
+      electronVersion,
+      version,
+      outputDir: BUILD_TMP_DIR,
+      replaceModuleDirs: true
+    });
     log("Done");
   } else {
-    if (! await makePackage("x64", "win32")) {
+    if (! await makePackage( {
+          arch: "x64",
+          platform: "win32",
+          electronVersion,
+          version,
+          outputDir: BUILD_TMP_DIR,
+          replaceModuleDirs: true
+        })) {
       return;
     }
     
-    if (! await makePackage("x64", "linux")) {
+    if (! await makePackage( {
+          arch: "x64",
+          platform: "linux",
+          electronVersion,
+          version,
+          outputDir: BUILD_TMP_DIR,
+          replaceModuleDirs: true
+        })) {
       return;
     }
 
-    if (! await makePackage("x64", "darwin")) {
+    if (! await makePackage( {
+          arch: "x64",
+          platform: "darwin",
+          electronVersion,
+          version,
+          outputDir: BUILD_TMP_DIR,
+          replaceModuleDirs: true
+        })) {
       return;
     }
 
