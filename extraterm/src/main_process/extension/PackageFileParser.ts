83147f2cd8ddf78a5ec31626e936eb75201a34d3
Simon Edwards <simon@simonzone.com>
2019-03-22 22:53:58 +0100

    Refactor extract a heap of Json-to-ast util functions.

--- extraterm/src/main_process/extension/PackageFileParser.ts
+++ extraterm/src/main_process/extension/PackageFileParser.ts
@@ -10,53 +10,12 @@ import { BooleanExpressionEvaluator } from "extraterm-boolean-expression-evaluat
 import { JsonNode, JsonObject } from "json-to-ast";
 import jsonParse = require("json-to-ast");
 
-const FONT_AWESOME_DEFAULT = false;
+import { JsonError, assertIsJsonObject, getJsonProperty, throwJsonError, parseObjectListJson, getJsonStringField, getJsonNumberField, getJsonStringArrayField, getJsonBooleanField } from "./JsonToAstUtils";
 
+const FONT_AWESOME_DEFAULT = false;
 
 export function parsePackageJsonString(packageJsonString: string, extensionPath: string): ExtensionMetadata {
   return (new PackageParser(extensionPath)).parse(packageJsonString);
-}
-
-class JsonError {
-  constructor(public readonly msg: string, public readonly node: JsonNode) {
-  }
-}
-
-function throwJsonError(msg: string, node: JsonNode): never {
-  throw new JsonError(msg, node);
-}
-
-function assertIsJsonObject(json: JsonNode): JsonObject {
-  if (json.type === "Object") {
-    return json;
-  }
-  return throwJsonError("Expected an object.", json);
-}
-
-function getJsonProperty(json: JsonObject, name: string): JsonNode {
-  for (const prop of json.children) {
-    if (name === prop.key.value) {
-      return prop.value;
-    }
-  }
-  return undefined;
-}
-
-function parseObjectListJson<T>(packageJson: JsonNode, fieldName: string, objectParser: (node: JsonNode) => T): T[] {
-  const jsonObject = assertIsJsonObject(packageJson);
-  const value = getJsonProperty(jsonObject, fieldName);
-  if (value == null) {
-    return [];
-  }
-
-  if (value.type === "Array") {
-    const result: T[] = [];
-    for (const item of value.children) {
-      result.push(objectParser(item));
-    }
-    return result;
-  }
-  return throwJsonError(`Field '${fieldName}' is not an array.`, value);
 }
 
 const categoryList: Category[] = [
@@ -94,13 +53,13 @@ class PackageParser {
       const packageJson = jsonParse(packageJsonString, {loc: true});
 
       const result: ExtensionMetadata = {
-        name: this.getJsonStringField(packageJson, "name"),
+        name: getJsonStringField(packageJson, "name"),
         path: this._extensionPath,
-        main: this.getJsonStringField(packageJson, "main", null),
-        version: this.getJsonStringField(packageJson, "version"),
+        main: getJsonStringField(packageJson, "main", null),
+        version: getJsonStringField(packageJson, "version"),
         includePlatform: this.parsePlatformsJson(packageJson, "includePlatform"),
         excludePlatform: this.parsePlatformsJson(packageJson, "excludePlatform"),
-        description: this.getJsonStringField(packageJson, "description"),
+        description: getJsonStringField(packageJson, "description"),
         contributes: this.parseContributesJson(packageJson)
       };
       return result;
@@ -114,44 +73,8 @@ class PackageParser {
     }
   }
 
-  private getJsonStringField(packageJson: JsonNode, fieldName: string, defaultValue: string=undefined): string {
-    const jsonObject = assertIsJsonObject(packageJson);
-    const value = getJsonProperty(jsonObject, fieldName);
-    if (value == null) {
-      if (defaultValue !== undefined) {
-        return defaultValue;
-      }
-      return throwJsonError(`Field '${fieldName}' is missing.`, jsonObject);
-    }
-
-    if (value.type === "Literal") {
-      if(typeof value.value === "string") {
-        return value.value;
-      }
-    }
-    return throwJsonError(`Field '${fieldName}' is not a string.`, value);
-  }
-
-  private getJsonNumberField(packageJson: JsonNode, fieldName: string, defaultValue: number=1000000): number {
-    const jsonObject = assertIsJsonObject(packageJson);
-    const value = getJsonProperty(jsonObject, fieldName);
-    if (value == null) {
-      if (defaultValue !== undefined) {
-        return defaultValue;
-      }
-      return throwJsonError(`Field '${fieldName}' is missing.`, jsonObject);
-    }
-
-    if (value.type === "Literal") {
-      if (typeof value.value === "number") {
-        return value.value;
-      }
-    }    
-    return throwJsonError(`Field '${fieldName}' is not a number.`, value);
-  }
-
   private getJsonCategoryField(packageJson: JsonNode, fieldName: string): Category {
-    const value = this.getJsonStringField(packageJson, fieldName, null);
+    const value = getJsonStringField(packageJson, fieldName, null);
     if (value == null) {
       return "window";
     }
@@ -161,49 +84,6 @@ class PackageParser {
         getJsonProperty(<JsonObject>packageJson, fieldName));
     }
     return <Category> value;
-  }
-
-  private getJsonBooleanField(packageJson: JsonNode, fieldName: string, defaultValue: boolean): boolean {
-    const jsonObject = assertIsJsonObject(packageJson);
-    const value = getJsonProperty(jsonObject, fieldName);
-    if (value == null) {
-      if (defaultValue !== undefined) {
-        return defaultValue;
-      }
-      return throwJsonError(`Field '${fieldName}' is missing.`, jsonObject);
-    }
-
-    if (value.type === "Literal") {
-      if (typeof value.value === "boolean") {
-        return value.value;
-      }
-    }    
-    return throwJsonError(`Field '${fieldName}' is not a boolean.`, value);
-  }
-
-  private getJsonStringArrayField(packageJson: JsonNode, fieldName: string, defaultValue: string[]=undefined): string[] {
-    const jsonObject = assertIsJsonObject(packageJson);
-    const value = getJsonProperty(jsonObject, fieldName);
-    if (value == null) {
-      if (defaultValue !== undefined) {
-        return defaultValue;
-      }
-      return throwJsonError(`Field '${fieldName}' is missing.`, packageJson);
-    }
-
-    if (value.type === "Array") {
-      const result: string[] = [];
-      for (let i=0; i < value.children.length; i++) {
-        const kid = value.children[i];
-        if (kid.type !== "Literal" || typeof kid.value !== "string") {
-          return throwJsonError(`Item of field '${fieldName}' is not a string.`, kid);
-        }
-        result.push(kid.value);
-      }
-      return result;
-    }
-
-    return throwJsonError(`Field '${fieldName}' is not an array.`, value);
   }
 
   private parsePlatformsJson(packageJson: JsonNode, fieldName: string): ExtensionPlatform[] {
@@ -226,8 +106,8 @@ class PackageParser {
 
   private parsePlatformJson(packageJson: JsonNode): ExtensionPlatform {
     return {
-      os: this.getJsonStringField(packageJson, "os", null),
-      arch: this.getJsonStringField(packageJson, "arch", null)
+      os: getJsonStringField(packageJson, "os", null),
+      arch: getJsonStringField(packageJson, "arch", null)
     };
   }
 
@@ -287,8 +167,8 @@ class PackageParser {
 
   private parseViewerConstributionJson(packageJson: JsonNode): ExtensionViewerContribution {
     return {
-      name: this.getJsonStringField(packageJson, "name"),
-      mimeTypes: this.getJsonStringArrayField(packageJson, "mimeTypes"),
+      name: getJsonStringField(packageJson, "name"),
+      mimeTypes: getJsonStringArrayField(packageJson, "mimeTypes"),
       css: this.parseCss(packageJson)
     };
   }
@@ -305,29 +185,29 @@ class PackageParser {
     }
 
     return {
-      directory: this.getJsonStringField(value, "directory", "."),
-      cssFile: this.getJsonStringArrayField(value, "cssFile", []),
-      fontAwesome: this.getJsonBooleanField(value, "fontAwesome", FONT_AWESOME_DEFAULT)
+      directory: getJsonStringField(value, "directory", "."),
+      cssFile: getJsonStringArrayField(value, "cssFile", []),
+      fontAwesome: getJsonBooleanField(value, "fontAwesome", FONT_AWESOME_DEFAULT)
     };
   }
 
   private parseCommandContributionJson(packageJson: JsonNode): ExtensionCommandContribution {
     return {
-      command: this.getJsonStringField(packageJson, "command"),
-      title: this.getJsonStringField(packageJson, "title"),
+      command: getJsonStringField(packageJson, "command"),
+      title: getJsonStringField(packageJson, "title"),
       when: this.getJsonWhenField(packageJson, "when", ""),
       category: this.getJsonCategoryField(packageJson, "category"),
-      order: this.getJsonNumberField(packageJson, "order", 100000),
-      commandPalette: this.getJsonBooleanField(packageJson, "commandPalette", true),
-      contextMenu: this.getJsonBooleanField(packageJson, "contextMenu", false),
-      emptyPaneMenu: this.getJsonBooleanField(packageJson, "emptyPaneMenu", false),
-      newTerminalMenu: this.getJsonBooleanField(packageJson, "newTerminalMenu", false),
-      icon: this.getJsonStringField(packageJson, "icon", ""),
+      order: getJsonNumberField(packageJson, "order", 100000),
+      commandPalette: getJsonBooleanField(packageJson, "commandPalette", true),
+      contextMenu: getJsonBooleanField(packageJson, "contextMenu", false),
+      emptyPaneMenu: getJsonBooleanField(packageJson, "emptyPaneMenu", false),
+      newTerminalMenu: getJsonBooleanField(packageJson, "newTerminalMenu", false),
+      icon: getJsonStringField(packageJson, "icon", ""),
     };
   }
 
   private getJsonWhenField(packageJson: JsonNode, fieldName: string, defaultValue: string=undefined): string {
-    const value = this.getJsonStringField(packageJson, fieldName, defaultValue);
+    const value = getJsonStringField(packageJson, fieldName, defaultValue);
 
     if (value.trim() === "") {
       return "true";
@@ -344,35 +224,35 @@ class PackageParser {
 
   private parseSessionEditorConstributionJson(packageJson: JsonNode): ExtensionSessionEditorContribution {
     return {
-      name: this.getJsonStringField(packageJson, "name"),
-      type: this.getJsonStringField(packageJson, "type"),
+      name: getJsonStringField(packageJson, "name"),
+      type: getJsonStringField(packageJson, "type"),
       css: this.parseCss(packageJson)
     };
   }
 
   private parseSessionBackendConstributionJson(packageJson: JsonNode): ExtensionSessionBackendContribution {
     return {
-      name: this.getJsonStringField(packageJson, "name"),
-      type: this.getJsonStringField(packageJson, "type")
+      name: getJsonStringField(packageJson, "name"),
+      type: getJsonStringField(packageJson, "type")
     };
   }
 
   private parseSyntaxThemeContributionsJson(packageJson: JsonNode): ExtensionSyntaxThemeContribution {
     return {
-      path: this.getJsonStringField(packageJson, "path")
+      path: getJsonStringField(packageJson, "path")
     };
   }
 
   private parseSyntaxThemeProviderContributionsJson(packageJson: JsonNode): ExtensionSyntaxThemeProviderContribution {
     return {
-      name: this.getJsonStringField(packageJson, "name"),
-      humanFormatNames: this.getJsonStringArrayField(packageJson, "humanFormatNames", [])
+      name: getJsonStringField(packageJson, "name"),
+      humanFormatNames: getJsonStringArrayField(packageJson, "humanFormatNames", [])
     };
   }
 
   private parseTerminalBorderWidgetContributionsJson(packageJson: JsonNode): ExtensionTerminalBorderContribution {
     return {
-      name: this.getJsonStringField(packageJson, "name"),
+      name: getJsonStringField(packageJson, "name"),
       border: this.getJsonBorderDirectionField(packageJson, "border"),
       css: this.parseCss(packageJson)
     };
@@ -398,20 +278,20 @@ class PackageParser {
 
   private parseTerminalThemeContributionsJson(packageJson: JsonNode): ExtensionTerminalThemeContribution {
     return {
-      path: this.getJsonStringField(packageJson, "path")
+      path: getJsonStringField(packageJson, "path")
     };
   }
 
   private parseTerminalThemeProviderContributionsJson(packageJson: JsonNode): ExtensionTerminalThemeProviderContribution {
     return {
-      name: this.getJsonStringField(packageJson, "name"),
-      humanFormatNames: this.getJsonStringArrayField(packageJson, "humanFormatNames", [])
+      name: getJsonStringField(packageJson, "name"),
+      humanFormatNames: getJsonStringArrayField(packageJson, "humanFormatNames", [])
     };
   }
 
   private parseKeybindingsContributionsJson(packageJson: JsonNode): ExtensionKeybindingsContribution {
     return {
-      path: this.getJsonStringField(packageJson, "path")
+      path: getJsonStringField(packageJson, "path")
     };
   }
 }
