59cf18dc8c2494277ec73b05dad6b302b82cbd29
Simon Edwards <simon@simonzone.com>
2019-12-07 13:50:52 +0100

    Expand `LigatureMarker` for marking ligatures in a `CharCellGrid` row

--- packages/extraterm-char-render-canvas/src/LigatureMarker.jest.ts
+++ packages/extraterm-char-render-canvas/src/LigatureMarker.jest.ts
@@ -4,6 +4,8 @@
  * This source code is licensed under the MIT license which is detailed in the LICENSE.txt file.
  */
 import "jest";
+import { CharCellGrid } from "extraterm-char-cell-grid";
+
 import { LigatureMarker } from "./LigatureMarker";
 
 
@@ -27,3 +29,19 @@ describe.each([
       done();
     });
   });
+
+test("Mark ligatures", done => {
+  const grid = new CharCellGrid(10, 5);
+  grid.setString(0, 0, "Foo --> Bar");
+
+  const marker = new LigatureMarker(validLigatures);
+  marker.markLigatures(grid, 0);
+
+  expect(grid.getLigature(0, 0)).toBe(0);
+  expect(grid.getLigature(1, 0)).toBe(0);
+  expect(grid.getLigature(2, 0)).toBe(0);
+  expect(grid.getLigature(3, 0)).toBe(0);
+  expect(grid.getLigature(4, 0)).toBe(3);
+  expect(grid.getLigature(5, 0)).toBe(0);
+  done();
+});
--- packages/extraterm-char-render-canvas/src/LigatureMarker.ts
+++ packages/extraterm-char-render-canvas/src/LigatureMarker.ts
@@ -53,11 +53,12 @@ export class LigatureMarker {
   /**
    * Detect ligatures at the start of a string
    * 
-   * @param str the string ot search in
+   * @param codePoints the string ot search in
    * @return the length of the ligature in code points at the start of the string. 0 means no ligature.
    */
-  getLigatureLength(str: string): number {
-    return this._getLigatureLength(0, stringToCodePointArray(str), this._searchTreeRoot, 0);
+  getLigatureLength(codePoints: string | Uint32Array, index=0): number {
+    const uint32CodePoints = typeof codePoints === "string" ? stringToCodePointArray(codePoints) : codePoints;
+    return this._getLigatureLength(index, uint32CodePoints, this._searchTreeRoot, 0);
   }
 
   private _getLigatureLength(index: number, codePoints: Uint32Array, searchTreeNode: SearchTreeNode, accu: number): number {
@@ -76,7 +77,21 @@ export class LigatureMarker {
     return this._getLigatureLength(index+1, codePoints, node, accu+1);
   }
 
-  markLigatures(grid: CharCellGrid): void {
-
+  /**
+   * Detect and mark the ligatures in CharCellGrid row
+   * 
+   * @param grid the grid to check and mark
+   * @parm row the row scan from start to end
+   */
+  markLigatures(grid: CharCellGrid, row: number): void {
+    const width = grid.width;
+    const codePoints = grid.getRowCodePoints(row);
+    for (let i=0; i<width; i++) {
+      const ligatureLength = this.getLigatureLength(codePoints, i);
+      grid.setLigature(i, row, ligatureLength);
+      if (ligatureLength !== 0) {
+        i += ligatureLength - 1;
+      }
+    }
   }
 }
