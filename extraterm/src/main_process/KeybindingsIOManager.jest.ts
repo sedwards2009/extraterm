e9ac64701156d1e9c1317727c1e44be8930c6759
Simon Edwards <simon@simonzone.com>
2020-04-19 17:21:16 +0200

    Add tests for KeybindingsIOManager

---
+++ extraterm/src/main_process/KeybindingsIOManager.jest.ts
@@ -0,0 +1,22 @@
+/*
+ * Copyright 2020 Simon Edwards <simon@simonzone.com>
+ *
+ * This source code is licensed under the MIT license which is detailed in the LICENSE.txt file.
+ */
+import 'jest';
+
+import { KeybindingsIOManager } from "./KeybindingsIOManager";
+import { MainExtensionManager } from './extension/MainExtensionManager';
+
+
+test("Scan & Flatten", () => {
+  const extensionManager = new MainExtensionManager(["../extensions"]);
+
+  const kbm = new KeybindingsIOManager(".", extensionManager);
+
+  const flatBindings = kbm.getFlatKeybindingsSet("pc-style");
+  expect(flatBindings.extends).toBe("pc-style");
+  expect(flatBindings.bindings.length).not.toBe(0);
+
+  expect(flatBindings.bindings.filter(b => b.command === "extraterm:window.newTerminal").length).toBe(1);
+});
