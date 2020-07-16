0f41d20324e53c28f0d7e4e376d1da0b2fb2e1c0
Simon Edwards <simon@simonzone.com>
2020-07-16 11:00:20 +0200

    [WebGL Renderer] Extract `FontSlice` into its own file

--- packages/extraterm-char-render-canvas/src/CharRenderCanvas.ts
+++ packages/extraterm-char-render-canvas/src/CharRenderCanvas.ts
@@ -14,6 +14,7 @@ import { RGBAToCss } from "./RGBAToCss";
 import { CPURenderedFontAtlas } from "./font_atlas/CPURenderedFontAtlas";
 import { ImageBitmapFontAtlas } from "./font_atlas/ImageBitmapFontAtlas";
 import { normalizedCellIterator, NormalizedCell } from "./NormalizedCellIterator";
+import { FontSlice } from "./FontSlice";
 
 export const PALETTE_BG_INDEX = 256;
 export const PALETTE_FG_INDEX = 257;
@@ -168,51 +169,6 @@ export interface CharRenderCanvasOptions {
    * The render method to use.
    */
   renderer?: Renderer;
-}
-
-export interface FontSlice {
-  /**
-   * Font family to render the cells using
-   *
-   * The exact name is the same as that required by CSS.
-   */
-  fontFamily: string;
-
-  /**
-   * Size of the font in pixels
-   */
-  fontSizePx: number;
-
-  /**
-   * Set to true if the font is a color font
-   */
-  isColor?: boolean;
-
-  /**
-   * Start code point of the unicode range
-   *
-   * This and `unicodeEnd` define the range of unicode code points for
-   * which this font is to be used.
-   */
-  unicodeStart?: number;
-
-  /**
-   * End code point of the unicode range (exclusive)
-   *
-   * This and `unicodeStart` define the range of unicode code points for
-   * which this font is to be used.
-   */
-  unicodeEnd?: number;
-
-  /**
-   * Characters used to determine the effective size of the glyphs
-   *
-   * These characters are rendered and examined on the pixel level to
-   * determine the actual size of the font on the screen.
-   */
-  sampleChars?: string[];
-
-  unicodeCodePoints?: number[];
 }
 
 interface ExtraFontSlice extends FontSlice {
--- packages/extraterm-char-render-canvas/src/index.ts
+++ packages/extraterm-char-render-canvas/src/index.ts
@@ -2,10 +2,12 @@
  * Copyright 2020 Simon Edwards <simon@simonzone.com>
  */
 
-export { xtermPalette, CharRenderCanvasOptions, CursorStyle, FontSlice, CharRenderCanvas, Renderer } from "./CharRenderCanvas";
+export { xtermPalette, CharRenderCanvasOptions, CursorStyle, CharRenderCanvas, Renderer } from "./CharRenderCanvas";
+export { FontSlice } from "./FontSlice";
 export { computeFontMetrics, computeDpiFontMetrics, debugFontMetrics } from "./font_metrics/FontMeasurement";
 export { MonospaceFontMetrics } from "./font_metrics/MonospaceFontMetrics";
 export { CPURenderedFontAtlasRepository, ImageBitmapFontAtlasRepository } from "./font_atlas/FontAtlasRepository";
 export { Disposable } from "./Disposable";
 export { WebGLRenderer } from "./WebGLRenderer";
 export { TextureFontAtlas } from "./font_atlas/TextureFontAtlas";
+export { WebGLCharRenderCanvas } from "./WebGLCharRenderCanvas";
