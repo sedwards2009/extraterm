34baecc88cebf8584f7dc68458c95dd524e44d2c
Simon Edwards <simon@simonzone.com>
2020-08-21 22:06:36 +0200

    Remove old rendering code

--- packages/extraterm-char-render-canvas/src/index.ts
+++ packages/extraterm-char-render-canvas/src/index.ts
@@ -1,14 +1,12 @@
 /**
  * Copyright 2020 Simon Edwards <simon@simonzone.com>
  */
-
-export { xtermPalette, CharRenderCanvasOptions, CharRenderCanvas, Renderer } from "./CharRenderCanvas";
 export { CursorStyle } from "./CursorStyle";
 export { FontSlice } from "./FontSlice";
 export { computeFontMetrics, computeDpiFontMetrics, debugFontMetrics } from "./font_metrics/FontMeasurement";
 export { MonospaceFontMetrics } from "./font_metrics/MonospaceFontMetrics";
-export { CPURenderedFontAtlasRepository, ImageBitmapFontAtlasRepository } from "./font_atlas/FontAtlasRepository";
 export { Disposable } from "./Disposable";
 export { WebGLRenderer } from "./WebGLRenderer";
 export { TextureFontAtlas, TextureCachedGlyph } from "./font_atlas/TextureFontAtlas";
 export { WebGLCharRenderCanvas } from "./WebGLCharRenderCanvas";
+export { xtermPalette,  } from "./WebGLCharRenderCanvas";
--- test/extraterm-char-render-canvas-test/src/main.ts
+++ test/extraterm-char-render-canvas-test/src/main.ts
@@ -1,5 +1,5 @@
 
-import { CharRenderCanvas, Renderer, xtermPalette, TextureFontAtlas, WebGLRenderer, computeFontMetrics, MonospaceFontMetrics, WebGLCharRenderCanvas } from "extraterm-char-render-canvas";
+import { xtermPalette, TextureFontAtlas, WebGLRenderer, computeFontMetrics, MonospaceFontMetrics, WebGLCharRenderCanvas } from "extraterm-char-render-canvas";
 import { CharCellGrid } from "extraterm-char-cell-grid";
 import { printTestPattern, printEmoji, CellGridOutputDevice, printPalette, VtOutputDevice, OutputDevice } from "./TestPattern";
 import { WebGLRendererRepository } from "extraterm-char-render-canvas/dist/WebGLRendererRepository";
@@ -11,23 +11,10 @@ const log = console.log.bind(console);
 
 const WIDTH_CHARS = 130;
 const HEIGHT_CHARS = 68;
-let renderCanvas: CharRenderCanvas = null;
 let webglRenderCanvas: WebGLCharRenderCanvas = null;
 
 function main(): void {
   log("Main!");
-
-  const renderButton1 = document.getElementById("render_button1");
-  renderButton1.addEventListener("click", renderTestPattern);
-
-  const renderButton2 = document.getElementById("render_button2");
-  renderButton2.addEventListener("click", renderLowerAlphaPattern);
-
-  const renderButton3 = document.getElementById("render_button3");
-  renderButton3.addEventListener("click", renderUpperAlphaPattern);
-
-  const renderButton4 = document.getElementById("render_button4");
-  renderButton4.addEventListener("click", renderEmoji);
 
   const renderButton5 = document.getElementById("render_button5");
   renderButton5.addEventListener("click", testWebGL);
@@ -41,65 +28,6 @@ function main(): void {
   const renderButton8 = document.getElementById("render_button8");
   renderButton8.addEventListener("click", renderWebGLUpperAlphaPattern);
 
-}
-
-function renderTestPattern(): void {
-  const containerDiv = document.getElementById("container");
-  createRenderCanvas();
-  printTestPattern(new CellGridOutputDevice(renderCanvas.getCellGrid()));
-  renderCanvas.render();
-  containerDiv.appendChild(renderCanvas.getFontAtlasCanvasElement());
-}
-
-function renderLowerAlphaPattern(): void {
-  const containerDiv = document.getElementById("container");
-  createRenderCanvas();
-  fillGridWithString(renderCanvas.getCellGrid(), "abcdefghijklmonpqrstuvwxyz");
-  renderCanvas.render();
-  containerDiv.appendChild(renderCanvas.getFontAtlasCanvasElement());
-}
-
-function renderUpperAlphaPattern(): void {
-  const containerDiv = document.getElementById("container");
-  createRenderCanvas();
-  fillGridWithString(renderCanvas.getCellGrid(), "ABCDEFGHIJKLMONPQRSTUVWXYZ");
-  renderCanvas.render();
-  containerDiv.appendChild(renderCanvas.getFontAtlasCanvasElement());
-}
-
-function renderEmoji(): void {
-  const containerDiv = document.getElementById("container");
-  createRenderCanvas();
-  printEmoji(new CellGridOutputDevice(renderCanvas.getCellGrid()));
-  renderCanvas.render();
-  containerDiv.appendChild(renderCanvas.getFontAtlasCanvasElement());
-}
-
-function createRenderCanvas(): void {
-  if (renderCanvas != null) {
-    return;
-  }
-
-  const containerDiv = document.getElementById("container");
-  renderCanvas = new CharRenderCanvas({
-    widthChars: WIDTH_CHARS,
-    heightChars: HEIGHT_CHARS,
-    fontFamily: "ligadejavusansmono",
-    fontSizePx: 16,
-    palette: xtermPalette(),
-    extraFonts: [
-      {
-        fontFamily: "twemoji",
-        fontSizePx: 16,
-        unicodeStart: 0x1f000,
-        unicodeEnd: 0x20000,
-        sampleChars: ["\u{1f600}"]  // Smile emoji
-      }
-    ],
-    renderer: Renderer.CPU,
-  });
-
-  containerDiv.appendChild(renderCanvas.getCanvasElement());
 }
 
 function fillGridWithString(cellGrid: CharCellGrid, str: string): void {
