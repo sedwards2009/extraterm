1eb0f2b1b0dc17216640ffb1a1c2621406e93c2c
Simon Edwards <simon@simonzone.com>
2020-11-06 18:53:56 +0100

    Add logger lifecycle logging to extraterm-logging.

--- packages/extraterm-logging/src/Logger.ts
+++ packages/extraterm-logging/src/Logger.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright 2014-2016 Simon Edwards <simon@simonzone.com>
+ * Copyright 2014-2020 Simon Edwards <simon@simonzone.com>
  *
  * This source code is licensed under the MIT license which is detailed in the LICENSE.txt file.
  */
@@ -15,6 +15,8 @@ const instanceCounter = new Map<string, number>();
 const instanceNames = new WeakMap<any, string>(); // Maps objects to the names used by their loggers.
 const loggersMap = new WeakMap<any, Logger>();
 
+let logLifecycle = false;
+
 export type Level = 'DEBUG' | 'INFO' | 'WARN' | 'SEVERE';
 
 
@@ -23,6 +25,19 @@ interface LogMessage {
   msg: string;
 }
 
+/**
+ * Turn on/off logging of object lifecycles.
+ *
+ * When this is turned on, the creation of new logger objects will be logged
+ * and also when they are garbage collected.
+ *
+ * Defaults to off.
+ *
+ * @param on Set this to `true` if object lifecycles
+ */
+export function setLogObjectLifecycles(on: boolean): void {
+  logLifecycle = on;
+}
 
 export interface LogWriter {
   write(level: Level, msg: string, ...opts: any[]): void;
@@ -32,43 +47,43 @@ export interface LogWriter {
 export interface Logger {
   /**
    * Get the unique name of this logger instance.
-   * 
+   *
    * @return the name of this instance
    */
   getName(): string;
 
   /**
    * Log a debug message.
-   * 
+   *
    * @param msg     the log message
    * @param ...opts extra values to log with the message
    */
   debug(msg: any, ...opts: any[]): void;
-  
+
   /**
    * Log an info message.
-   * 
+   *
    * @param msg     the log message
    * @param ...opts extra values to log with the message
    */
   info(msg: any, ...opts: any[]): void;
-  
+
   /**
    * Log a warning message.
-   * 
+   *
    * @param msg     the log message
    * @param ...opts extra values to log with the message
    */
   warn(msg: any, ...opts: any[]): void;
-  
+
   /**
    * Log a severe message.
-   * 
+   *
    * @param msg     the log message
    * @param ...opts extra values to log with the message
    */
   severe(msg: any, ...opts: any[]): void;
-  
+
   /**
    * Turns on recording log messages to an internal buffer.
    *
@@ -76,14 +91,14 @@ export interface Logger {
    * See `stopRecording()` and `getLogMessages()`.
    */
   startRecording(): void;
-  
+
   /**
    * Turns off recording log messages to the internal buffer.
    *
    * See `startRecording()` and `getLogMessages()`.
    */
   stopRecording(): void;
-  
+
   /**
    * Starts timing.
    *
@@ -92,7 +107,7 @@ export interface Logger {
    * @param label identifying label for this timer
    */
   startTime(label: string): void;
-  
+
   /**
    * Ends timing.
    *
@@ -111,23 +126,41 @@ export interface Logger {
 }
 
 
+const finalizationRegistry = new FinalizationRegistry( (log: Logger) => {
+  log.debug(`Object destroyed.`);
+});
+
+
 export function getLogger(name?: string, instance?: any): Logger {
   if (instance != null) {
     if (loggersMap.has(instance)) {
       return loggersMap.get(instance);
     }
-    const logger = new LoggerImpl(name, instance);
+
+    const baseName = name === undefined ? "(unknown)" : name;
+    const instanceCount = instanceCounter.has(baseName) ? instanceCounter.get(baseName) + 1 : 0;
+    instanceCounter.set(baseName, instanceCount);
+    const loggerName = baseName + " #" + instanceCount;
+
+    if (instance != null) {
+      instanceNames.set(instance, loggerName);
+    }
+    const logger = new LoggerImpl(loggerName);
     loggersMap.set(instance, logger);
+    if (logLifecycle) {
+      finalizationRegistry.register(instance, logger);
+      logger.debug("Object created.");
+    }
     return logger;
   }
-  
-  return new LoggerImpl(name, instance);
+
+  return new LoggerImpl(name);
 }
 
 
 /**
  * Try to map an object to a human understandable name.
- * 
+ *
  * @param obj the object
  * @return the name of the object or null if one could not be found.
  */
@@ -145,30 +178,18 @@ export function addLogWriter(writer: LogWriter): void {
 
 
 class LoggerImpl implements Logger {
-  
-  private _name: string;
-  
   private _recording = false;
-
   private _messageLog: LogMessage[] = [];
 
   /**
-   * Contruct a logger.
-   * 
+   * Construct a logger.
+   *
    * @param  name the name of the code or class associated with this logger instance.
    * @return the new logger instance
    */
-  constructor(name?: string, instance?: any) {
-    const baseName = name === undefined ? "(unknown)" : name;
-    const instanceCount = instanceCounter.has(baseName) ? instanceCounter.get(baseName) + 1 : 0;
-    instanceCounter.set(baseName, instanceCount);
-    this._name = baseName + " #" + instanceCount;
-
-    if (instance != null) {
-      instanceNames.set(instance, this._name);
-    }
+  constructor(private _name: string) {
   }
-  
+
   getName(): string {
     return this._name;
   }
@@ -176,39 +197,39 @@ class LoggerImpl implements Logger {
   debug(msg: any, ...opts: any[]): void {
     this._logMessage("DEBUG", msg, opts);
   }
-  
+
   info(msg: any, ...opts: any[]): void {
     this._logMessage("INFO", msg, opts);
   }
-  
+
   warn(msg: any, ...opts: any[]): void {
     this._logMessage("WARN", msg, opts);
   }
-  
+
   severe(msg: any, ...opts: any[]): void {
     this._logMessage("SEVERE", msg, opts);
   }
-  
+
   startRecording(): void {
-    this._recording = true;    
+    this._recording = true;
   }
-  
+
   stopRecording(): void {
     this._recording = false;
   }
-  
+
   startTime(label: string): void {
     console.time(label);
   }
-  
+
   endTime(label: string): void {
     console.timeEnd(label);
-  }  
-  
+  }
+
   getFormattedLogMessages(): string {
     return this._messageLog.reduce( (accu, logMessage) => accu + "\n" + logMessage.msg, "");
   }
-  
+
   private _logMessage(level: Level, msg: string, opts: any[]): void {
     const formatted = this._format(level, msg);
     if (this._recording) {
@@ -219,12 +240,11 @@ class LoggerImpl implements Logger {
       logWriters[i].write(level, formatted, ...opts);
     }
   }
-  
+
   private _format(level: string, msg: string): string {
     return `${(new Date()).toISOString().replace(/(T|Z)/g," ").trim()} ${level} [${this._name}] ${msg}`;
   }
 }
-
 
 class ConsoleLogWriter implements LogWriter {
   write(level: Level, msg: string, ...opts: any[]): void {
@@ -242,4 +262,5 @@ class ConsoleLogWriter implements LogWriter {
     }
   }
 }
+
 addLogWriter(new ConsoleLogWriter());
