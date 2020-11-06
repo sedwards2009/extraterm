2e8d6c0995a5061b13956fd81e62efd6ee3825aa
Simon Edwards <simon@simonzone.com>
2020-11-06 15:53:54 +0100

    Turn `MutationObserver` on/off during DOM connect/disconnect in `Tab.ts`.

--- extraterm/src/render_process/gui/Tab.ts
+++ extraterm/src/render_process/gui/Tab.ts
@@ -1,9 +1,9 @@
 /*
- * Copyright 2014-2016 Simon Edwards <simon@simonzone.com>
+ * Copyright 2014-2020 Simon Edwards <simon@simonzone.com>
  *
  * This source code is licensed under the MIT license which is detailed in the LICENSE.txt file.
  */
-import {log, Logger, getLogger} from "extraterm-logging";
+import { Logger, getLogger} from "extraterm-logging";
 import { CustomElement } from 'extraterm-web-component-decorators';
 
 /**
@@ -24,11 +24,15 @@ export class Tab extends HTMLElement {
     this._mutationObserver = new MutationObserver( (mutations) => {
       this._applyDraggable();
     });
-    this._mutationObserver.observe(this, { childList: true });
   }
 
   connectedCallback(): void {
     this._applyDraggable();
+    this._mutationObserver.observe(this, { childList: true });
+  }
+
+  disconnectedCallback(): void {
+    this._mutationObserver.disconnect();
   }
 
   private _applyDraggable(): void {
