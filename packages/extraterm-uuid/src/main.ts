5f85979565184e4615084c15192de19477b0d78e
Simon Edwards <simon@simonzone.com>
2021-05-19 23:31:51 +0200

    Carefully check for non-null `window` in `createUuid()`

--- packages/extraterm-uuid/src/main.ts
+++ packages/extraterm-uuid/src/main.ts
@@ -12,7 +12,7 @@
  */
 export function createUuid(): string {
   const buffer = new Uint8Array(16);
-  if (window?.crypto?.getRandomValues != null) {
+  if (window != null && window?.crypto?.getRandomValues != null) {
     window.crypto.getRandomValues(buffer);
   } else {
     const crypto = require("crypto");
