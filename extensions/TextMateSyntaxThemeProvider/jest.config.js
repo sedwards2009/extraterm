fb519b7189454150bcded3c224ef6e1a9c47653b
Simon Edwards <simon@simonzone.com>
2021-06-20 11:32:11 +0200

    Delete a bunch of syntax theme related extensions

--- extensions/TextMateSyntaxThemeProvider/jest.config.js
+++
@@ -1,7 +0,0 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
-module.exports = {
-  rootDir: "dist",
-  moduleFileExtensions: ["js"],
-  testMatch: ["**/*.jest.js"],
-};
