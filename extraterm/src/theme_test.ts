32f925548843d84170ab14861e507ebc3e04708a
Simon Edwards <simon@simonzone.com>
2021-07-14 21:20:07 +0200

    Updates for the DarkTwo style

--- extraterm/src/theme_test.ts
+++ extraterm/src/theme_test.ts
@@ -1,5 +1,6 @@
 import { Direction, QApplication, QMainWindow, QWidget } from "@nodegui/nodegui";
-import { BoxLayout, CheckBox, ComboBox, Label, LineEdit, PushButton, SpinBox, Widget } from "qt-construct";
+import { BoxLayout, CheckBox, ComboBox, Label, LineEdit, PushButton, RadioButton, ScrollArea, SpinBox, TabWidget,
+  Widget } from "qt-construct";
 import * as fs from "fs";
 
 let centralWidget: QWidget = null;
@@ -7,22 +8,61 @@ let centralWidget: QWidget = null;
 function main(): void {
   const win = new QMainWindow();
   win.setWindowTitle("Theme Test");
-  centralWidget = new QWidget();
-  centralWidget.setLayout(
-    BoxLayout({
-      direction: Direction.TopToBottom,
-      children: [
-        PushButton({text: "Reload Styles", onClicked: loadStyle}),
-        Label({text: "Hello"}),
-        CheckBox({text: "I like checkboxes"}),
 
-        ComboBox({items: ["Pizza", "Chips"]}),
-        SpinBox({suffix: " frames", minimum: 0, maximum: 1000}),
-        LineEdit({text: "Some text"}),
-        { widget: Widget({}), stretch: 1}
-      ]
+  centralWidget = ScrollArea({
+    widgetResizable: true,
+    widget: Widget({
+      layout: BoxLayout({
+        direction: Direction.TopToBottom,
+        children: [
+          PushButton({text: "Reload Styles", onClicked: loadStyle}),
+          Label({text: "H1 Heading", cssClass: "h1"}),
+          Label({text: "H2 Heading", cssClass: "h2"}),
+          Label({text: "H3 Heading", cssClass: "h3"}),
+          Label({text: "H4 Heading", cssClass: "h4"}),
+          Label({text: "H5 Heading", cssClass: "h5"}),
+          Label({text: "H6 Heading", cssClass: "h6"}),
+          Label({text: "Plain QLabel text"}),
+
+          PushButton({text: "Default button"}),
+          PushButton({text: "Success button", cssClass: "success"}),
+          PushButton({text: "Info button", cssClass: "info"}),
+          PushButton({text: "Warning button", cssClass: "warning"}),
+          PushButton({text: "Danger button", cssClass: "danger"}),
+
+          PushButton({text: "Disabled default button", enabled: false}),
+          PushButton({text: "Disabled success button", enabled: false, cssClass: "success"}),
+          PushButton({text: "Disabled info button", enabled: false, cssClass: "info"}),
+          PushButton({text: "Disabled warning button", enabled: false, cssClass: "warning"}),
+          PushButton({text: "Disabled danger button", enabled: false, cssClass: "danger"}),
+
+          PushButton({text: "Small Default button", cssClass: "small"}),
+          PushButton({text: "Small Success button", cssClass: ["small", "success"]}),
+          PushButton({text: "Small Info button", cssClass: ["small", "info"]}),
+          PushButton({text: "Small Warning button", cssClass: ["small", "warning"]}),
+          PushButton({text: "Small Danger button", cssClass: ["small", "danger"]}),
+
+          LineEdit({text: "Some text input"}),
+
+
+          ComboBox({items: ["One", "Two"]}),
+
+          CheckBox({text: "Checkbox"}),
+          CheckBox({text: "Select Checkbox", checkState: true}),
+
+          RadioButton({text: "Radio button"}),
+          RadioButton({text: "Selected Radio button", checked: true}),
+
+          SpinBox({suffix: " frames", minimum: 0, maximum: 1000}),
+          TabWidget({tabs: [
+            {label: "Extraterm", page: Widget({})},
+            {label: "Tabs", page: Widget({})},
+          ]}),
+          { widget: Widget({}), stretch: 1}
+        ]
+      })
     })
-  );
+  });
 
   loadStyle();
   win.setCentralWidget(centralWidget);
@@ -32,7 +72,7 @@ function main(): void {
 }
 
 function loadStyle(): void {
-  const styleSheet = fs.readFileSync("c:/devel/extraterm/extraterm/src/dark_two.css", {encoding: "utf8"});
+  const styleSheet = fs.readFileSync("/home/sbe/devel/extraterm_qt/extraterm/src/dark_two.css", {encoding: "utf8"});
   centralWidget.setStyleSheet(styleSheet);
 }
 
