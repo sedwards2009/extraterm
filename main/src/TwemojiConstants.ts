f947d9efcb680f20e2319320d975dcbe3cb2224b
Simon Edwards <simon@simonzone.com>
2022-06-12 09:55:02 +0200

    Add missing file

---
+++ main/src/TwemojiConstants.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright 2022 Simon Edwards <simon@simonzone.com>
+ *
+ * This source code is licensed under the MIT license which is detailed in the LICENSE.txt file.
+ */
+
+export const TWEMOJI_FAMILY = "twemoji";
+export const TWEMOJI_UNICODE_START = 0x1f000;
+export const TWEMOJI_UNICODE_END = 0x20000;
